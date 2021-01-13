d1b349571d48dc8dbd1593a86e567d817ebcfe23
shimoncohen <shimoncohen655@gmail.com>
2021-01-13 16:25:34 +0200

    test: fix mock data of window._env_

--- src/__mocks__/confEnvShim.js
+++ src/__mocks__/confEnvShim.js
@@ -12,6 +12,32 @@ if (!window._env_) {
       LOCALE: {
         DATE_TIME_FORMAT: 'DD/MM/YYYY HH:mm',
       },
+      ACTIVE_LAYER_PROPERTIES: {
+        urlPattern:
+          'arcgis/rest/services/Demographics/USA_Population_Density/MapServer/WMTS',
+        urlPatternParams: {
+          service: 'WMTS',
+          layers: 'USGSShadedReliefOnly',
+          tiled: 'true',
+          matrixSet: 'default028mm',
+          style: 'default',
+          projection: 'EPSG:3857',
+          format: 'image/png',
+        },
+      },
+      MAP: {
+        center: [34.811, 31.908],
+        zoom: 14,
+      },
+      LOGGER: {
+        level: 'warn',
+        log2console: false,
+        log2httpServer: {
+          host: '',
+          port: '',
+          path: '',
+        },
+      },
     };
   })(void 0);
 }
