f351fa611e0b5c5207965d23e3cf1f95b562aae6
alebinson <newdarkvirus@gmail.com>
2021-06-13 11:02:39 +0300

    feat: layer details copy link

--- src/discrete-layer/components/layer-details/field-value-presentors/url.value-presentors.tsx
+++ src/discrete-layer/components/layer-details/field-value-presentors/url.value-presentors.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box } from '@map-colonies/react-components';
+import { IconButton } from '@map-colonies/react-core';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 import './url.value-presentors.css';
 
@@ -8,9 +10,17 @@ interface UrlValuePresentorProps {
 }
 
 export const UrlValuePresentorComponent: React.FC<UrlValuePresentorProps> = ({value}) => {
+  const [copied, setCopied] = useState<boolean>(false);
   return (
-    <Box className="detailsFieldValue detailsUrlFieldValue">
-      {value}
-    </Box> 
+    <>
+      <Box className="detailsFieldValue detailsUrlFieldValue">
+        {value}
+      </Box>
+      <Box className="detailsUrlFieldUrlCopy">
+        <CopyToClipboard text={value as string} onCopy={(): void => setCopied(true)}>
+          <IconButton icon="content_copy" />
+        </CopyToClipboard>
+      </Box>
+    </>
   );
 }
