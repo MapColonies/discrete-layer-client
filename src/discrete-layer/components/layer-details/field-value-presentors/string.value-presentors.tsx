268e7ee3e669484c5fd7ffe3a091d32a42ceb6b9
Ella <ellamartirosyan@gmail.com>
2021-06-27 10:21:44 +0300

    fix: unused const
    Unused const

--- src/discrete-layer/components/layer-details/field-value-presentors/string.value-presentors.tsx
+++ src/discrete-layer/components/layer-details/field-value-presentors/string.value-presentors.tsx
@@ -5,8 +5,6 @@ import { Mode } from '../../../../common/models/mode.enum';
 import { TextField } from '@map-colonies/react-core';
 import { IRecordFieldInfo } from '../layer-details.field-info';
 
-const FIRST_CHAR_IDX = 0;
-
 interface StringValuePresentorProps {
   mode: Mode;
   fieldInfo: IRecordFieldInfo;
@@ -15,7 +13,7 @@ interface StringValuePresentorProps {
 }
 
 export const StringValuePresentorComponent: React.FC<StringValuePresentorProps> = ({ mode, fieldInfo, value, formik }) => {
-  if (mode === Mode.VIEW || (mode === Mode.EDIT && fieldInfo.isManuallyEditable !== true)) {
+  if (formik === undefined || mode === Mode.VIEW || (mode === Mode.EDIT && fieldInfo.isManuallyEditable !== true)) {
     return (
       <Box className="detailsFieldValue">
         {value}
@@ -23,13 +21,14 @@ export const StringValuePresentorComponent: React.FC<StringValuePresentorProps> 
     );
   } else {
     // eslint-disable-next-line
-    const value = get(formik,`values[${fieldInfo.fieldName as string}]`);
+    let value = get(formik,`values[${fieldInfo.fieldName as string}]`);
     return (
       <Box className="detailsFieldValue">
         <TextField
           id={fieldInfo.fieldName as string}
           name={fieldInfo.fieldName as string}
           type="text"
+          // eslint-disable-next-line
           value={value}
         />
       </Box>
