78f532a9ef5b3fced7a51c961b6b2d8adc00953f
alebinson <newdarkvirus@gmail.com>
2021-06-29 15:29:54 +0300

    fix: models and translations

--- src/discrete-layer/models/Layer3DRecordModel.base.ts
+++ src/discrete-layer/models/Layer3DRecordModel.base.ts
@@ -22,35 +22,32 @@ export const Layer3DRecordModelBase = ModelBase
     __typename: types.optional(types.literal("Layer3DRecord"), "Layer3DRecord"),
     type: types.union(types.undefined, types.null, RecordTypeEnumType),
     classification: types.union(types.undefined, types.null, types.string),
-    productId: types.union(types.undefined, types.string),
     productName: types.union(types.undefined, types.string),
-    productVersion: types.union(types.undefined, types.null, types.string),
-    productType: types.union(types.undefined, types.string),
     description: types.union(types.undefined, types.null, types.string),
     srsId: types.union(types.undefined, types.null, types.string),
-    srsName: types.union(types.undefined, types.null, types.string),
     producerName: types.union(types.undefined, types.null, types.string),
     creationDate: types.union(types.undefined, types.null, types.frozen()),
     ingestionDate: types.union(types.undefined, types.null, types.frozen()),
     updateDate: types.union(types.undefined, types.null, types.frozen()),
     sourceDateStart: types.union(types.undefined, types.null, types.frozen()),
     sourceDateEnd: types.union(types.undefined, types.null, types.frozen()),
-    resolution: types.union(types.undefined, types.null, types.number),
     accuracyCE90: types.union(types.undefined, types.null, types.number),
     sensorType: types.union(types.undefined, types.null, types.array(SensorTypeEnumType)),
     region: types.union(types.undefined, types.null, types.string),
     projectName: types.union(types.undefined, types.null, types.string),
+    validationDate: types.union(types.undefined, types.null, types.frozen()),
+    version: types.union(types.undefined, types.null, types.string),
+    centroid: types.union(types.undefined, types.null, types.string),
     footprint: types.union(types.undefined, types.null, types.frozen()),
+    relativeAccuracyCE90: types.union(types.undefined, types.null, types.number),
+    estimatedPrecision: types.union(types.undefined, types.null, types.number),
+    measuredPrecision: types.union(types.undefined, types.null, types.number),
     nominalResolution: types.union(types.undefined, types.null, types.number),
     accuracyLE90: types.union(types.undefined, types.null, types.number),
-    accuracySE90: types.union(types.undefined, types.null, types.number),
-    visualAccuracy: types.union(types.undefined, types.null, types.number),
-    heightRange: types.union(types.undefined, types.null, types.number),
-    srsOrigin: types.union(types.undefined, types.null, types.string),
-    flightAlt: types.union(types.undefined, types.null, types.number),
     //id: types.union(types.undefined, types.string),
     id: types.identifier, //Alex change till proper deffs
     insertDate: types.union(types.undefined, types.null, types.frozen()),
+    wktGeometry: types.union(types.undefined, types.null, types.string),
     keywords: types.union(types.undefined, types.null, types.string),
     links: types.union(types.undefined, types.null, types.array(types.late((): any => LinkModel))),
   })
@@ -63,34 +60,31 @@ export const Layer3DRecordModelBase = ModelBase
 export class Layer3DRecordModelSelector extends QueryBuilder {
   get type() { return this.__attr(`type`) }
   get classification() { return this.__attr(`classification`) }
-  get productId() { return this.__attr(`productId`) }
   get productName() { return this.__attr(`productName`) }
-  get productVersion() { return this.__attr(`productVersion`) }
-  get productType() { return this.__attr(`productType`) }
   get description() { return this.__attr(`description`) }
   get srsId() { return this.__attr(`srsId`) }
-  get srsName() { return this.__attr(`srsName`) }
   get producerName() { return this.__attr(`producerName`) }
   get creationDate() { return this.__attr(`creationDate`) }
   get ingestionDate() { return this.__attr(`ingestionDate`) }
   get updateDate() { return this.__attr(`updateDate`) }
   get sourceDateStart() { return this.__attr(`sourceDateStart`) }
   get sourceDateEnd() { return this.__attr(`sourceDateEnd`) }
-  get resolution() { return this.__attr(`resolution`) }
   get accuracyCE90() { return this.__attr(`accuracyCE90`) }
   get sensorType() { return this.__attr(`sensorType`) }
   get region() { return this.__attr(`region`) }
   get projectName() { return this.__attr(`projectName`) }
+  get validationDate() { return this.__attr(`validationDate`) }
+  get version() { return this.__attr(`version`) }
+  get centroid() { return this.__attr(`centroid`) }
   get footprint() { return this.__attr(`footprint`) }
+  get relativeAccuracyCE90() { return this.__attr(`relativeAccuracyCE90`) }
+  get estimatedPrecision() { return this.__attr(`estimatedPrecision`) }
+  get measuredPrecision() { return this.__attr(`measuredPrecision`) }
   get nominalResolution() { return this.__attr(`nominalResolution`) }
   get accuracyLE90() { return this.__attr(`accuracyLE90`) }
-  get accuracySE90() { return this.__attr(`accuracySE90`) }
-  get visualAccuracy() { return this.__attr(`visualAccuracy`) }
-  get heightRange() { return this.__attr(`heightRange`) }
-  get srsOrigin() { return this.__attr(`srsOrigin`) }
-  get flightAlt() { return this.__attr(`flightAlt`) }
   get id() { return this.__attr(`id`) }
   get insertDate() { return this.__attr(`insertDate`) }
+  get wktGeometry() { return this.__attr(`wktGeometry`) }
   get keywords() { return this.__attr(`keywords`) }
   links(builder?: string | LinkModelSelector | ((selector: LinkModelSelector) => LinkModelSelector)) { return this.__child(`links`, LinkModelSelector, builder) }
 }
@@ -98,4 +92,4 @@ export function selectFromLayer3DRecord() {
   return new Layer3DRecordModelSelector()
 }
 
-export const layer3DRecordModelPrimitives = selectFromLayer3DRecord().type.classification.productId.productName.productVersion.productType.description.srsId.srsName.producerName.creationDate.ingestionDate.updateDate.sourceDateStart.sourceDateEnd.resolution.accuracyCE90.sensorType.region.projectName.footprint.nominalResolution.accuracyLE90.accuracySE90.visualAccuracy.heightRange.srsOrigin.flightAlt.insertDate.keywords.id.links(linkModelPrimitives)
+export const layer3DRecordModelPrimitives = selectFromLayer3DRecord().type.classification.productName.description.srsId.producerName.creationDate.ingestionDate.updateDate.sourceDateStart.sourceDateEnd.accuracyCE90.sensorType.region.projectName.validationDate.version.centroid.footprint.relativeAccuracyCE90.estimatedPrecision.measuredPrecision.nominalResolution.accuracyLE90.insertDate.wktGeometry.keywords.id.links(linkModelPrimitives)
--- src/discrete-layer/models/LayerRasterRecordModel.base.ts
+++ src/discrete-layer/models/LayerRasterRecordModel.base.ts
@@ -22,23 +22,23 @@ export const LayerRasterRecordModelBase = ModelBase
     __typename: types.optional(types.literal("LayerRasterRecord"), "LayerRasterRecord"),
     type: types.union(types.undefined, types.null, RecordTypeEnumType),
     classification: types.union(types.undefined, types.null, types.string),
-    productId: types.union(types.undefined, types.string),
     productName: types.union(types.undefined, types.string),
-    productVersion: types.union(types.undefined, types.null, types.string),
-    productType: types.union(types.undefined, types.string),
     description: types.union(types.undefined, types.null, types.string),
     srsId: types.union(types.undefined, types.null, types.string),
-    srsName: types.union(types.undefined, types.null, types.string),
     producerName: types.union(types.undefined, types.null, types.string),
     creationDate: types.union(types.undefined, types.null, types.frozen()),
     ingestionDate: types.union(types.undefined, types.null, types.frozen()),
     updateDate: types.union(types.undefined, types.null, types.frozen()),
     sourceDateStart: types.union(types.undefined, types.null, types.frozen()),
     sourceDateEnd: types.union(types.undefined, types.null, types.frozen()),
-    resolution: types.union(types.undefined, types.null, types.number),
     accuracyCE90: types.union(types.undefined, types.null, types.number),
     sensorType: types.union(types.undefined, types.null, types.array(SensorTypeEnumType)),
     region: types.union(types.undefined, types.null, types.string),
+    productId: types.union(types.undefined, types.string),
+    productVersion: types.union(types.undefined, types.null, types.string),
+    productType: types.union(types.undefined, types.string),
+    srsName: types.union(types.undefined, types.null, types.string),
+    resolution: types.union(types.undefined, types.null, types.number),
     rms: types.union(types.undefined, types.null, types.number),
     scale: types.union(types.undefined, types.null, types.string),
     footprint: types.union(types.undefined, types.null, types.frozen()),
@@ -58,23 +58,23 @@ export const LayerRasterRecordModelBase = ModelBase
 export class LayerRasterRecordModelSelector extends QueryBuilder {
   get type() { return this.__attr(`type`) }
   get classification() { return this.__attr(`classification`) }
-  get productId() { return this.__attr(`productId`) }
   get productName() { return this.__attr(`productName`) }
-  get productVersion() { return this.__attr(`productVersion`) }
-  get productType() { return this.__attr(`productType`) }
   get description() { return this.__attr(`description`) }
   get srsId() { return this.__attr(`srsId`) }
-  get srsName() { return this.__attr(`srsName`) }
   get producerName() { return this.__attr(`producerName`) }
   get creationDate() { return this.__attr(`creationDate`) }
   get ingestionDate() { return this.__attr(`ingestionDate`) }
   get updateDate() { return this.__attr(`updateDate`) }
   get sourceDateStart() { return this.__attr(`sourceDateStart`) }
   get sourceDateEnd() { return this.__attr(`sourceDateEnd`) }
-  get resolution() { return this.__attr(`resolution`) }
   get accuracyCE90() { return this.__attr(`accuracyCE90`) }
   get sensorType() { return this.__attr(`sensorType`) }
   get region() { return this.__attr(`region`) }
+  get productId() { return this.__attr(`productId`) }
+  get productVersion() { return this.__attr(`productVersion`) }
+  get productType() { return this.__attr(`productType`) }
+  get srsName() { return this.__attr(`srsName`) }
+  get resolution() { return this.__attr(`resolution`) }
   get rms() { return this.__attr(`rms`) }
   get scale() { return this.__attr(`scale`) }
   get footprint() { return this.__attr(`footprint`) }
@@ -88,4 +88,4 @@ export function selectFromLayerRasterRecord() {
   return new LayerRasterRecordModelSelector()
 }
 
-export const layerRasterRecordModelPrimitives = selectFromLayerRasterRecord().type.classification.productId.productName.productVersion.productType.description.srsId.srsName.producerName.creationDate.ingestionDate.updateDate.sourceDateStart.sourceDateEnd.resolution.accuracyCE90.sensorType.region.rms.scale.footprint.layerPolygonParts.insertDate.keywords.id.links(linkModelPrimitives)
+export const layerRasterRecordModelPrimitives = selectFromLayerRasterRecord().type.classification.productName.description.srsId.producerName.creationDate.ingestionDate.updateDate.sourceDateStart.sourceDateEnd.accuracyCE90.sensorType.region.productId.productVersion.productType.srsName.resolution.rms.scale.footprint.layerPolygonParts.insertDate.keywords.id.links(linkModelPrimitives)
