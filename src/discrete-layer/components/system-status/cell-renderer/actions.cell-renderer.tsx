7b4e4a1a39cc1ee264310309d5cb8e015f33184b
alebinson <newdarkvirus@gmail.com>
2021-07-12 11:53:34 +0300

    feat: disable actions if not allowed by status

--- src/discrete-layer/components/system-status/cell-renderer/actions.cell-renderer.tsx
+++ src/discrete-layer/components/system-status/cell-renderer/actions.cell-renderer.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { ICellRendererParams } from 'ag-grid-community';
 import { Box } from '@map-colonies/react-components';
+import { JobModelType, Status } from '../../../models';
 
 export const ActionsRenderer: React.FC<ICellRendererParams> = (props) => {
+  const isChangePriorityEnabled = (data: JobModelType): boolean => {
+    return data.status === Status.InProgress || data.status === Status.Pending
+  };
+
   return (
     <Box style={{
       display: 'flex',
       justifyContent: 'center'
     }}>
       <Box>
-        <button>Priority</button>
+        <button disabled={!isChangePriorityEnabled(props.data)}>Priority</button>
       </Box>
     </Box>
  );
