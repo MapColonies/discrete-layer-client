ca2a15707348cfb224caa848c88304bfb69755a8
alebinson <newdarkvirus@gmail.com>
2021-07-18 14:18:29 +0300

    fix: lint

--- src/discrete-layer/components/system-status/cell-renderer/status.cell-renderer.tsx
+++ src/discrete-layer/components/system-status/cell-renderer/status.cell-renderer.tsx
@@ -5,9 +5,11 @@ import { JobModelType, Status, TaskModelType } from '../../../models';
 
 import './status.cell-renderer.css';
 
+const NO_DATA = 0;
+
 export const StatusRenderer: React.FC<ICellRendererParams> = (props) => {
   const getProgress = (data: JobModelType): string => {
-    if(!data.tasks || data.tasks.length === 0){
+    if(!data.tasks || data.tasks.length === NO_DATA){
       return '';
     }
     const completed = data.tasks.filter((task: TaskModelType)=> task.status === Status.Completed);
