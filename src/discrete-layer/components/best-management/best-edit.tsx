23482de752a38bd92af15386bac2c8244f71e4ea
Ella <ellamartirosyan@gmail.com>
2021-07-29 10:45:05 +0300

    feat: div
    div

--- src/discrete-layer/components/best-management/best-edit.tsx
+++ src/discrete-layer/components/best-management/best-edit.tsx
@@ -10,11 +10,9 @@ export const BestEditComponent: React.FC = observer(() => {
   const bestDiscretes = store.discreteLayersStore.tabViews[0].layersImages as LayerRasterRecordModelType[];
 
   return (
-    <div style={{height: '100%'}}>
-      <BestDiscretesComponent 
-        discretes={ bestDiscretes } 
-        style={{ height: 'calc(100% - 80px)', width: 'calc(100% - 8px)' }}
-      />
-    </div>
-  )
+    <BestDiscretesComponent 
+      discretes={ bestDiscretes } 
+      style={{ height: 'calc(100% - 80px)', width: 'calc(100% - 8px)' }}
+    />
+  );
 })
