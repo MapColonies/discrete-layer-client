7b8647727cdea99f68400849a58aa9796fafedaa
Ella <ellamartirosyan@gmail.com>
2021-08-11 08:17:22 +0300

    feat: loading
    loading

--- src/common/components/tree/statuses/Loading.tsx
+++ src/common/components/tree/statuses/Loading.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { CircularProgress } from '@map-colonies/react-core';
 import { Box } from '@map-colonies/react-components';
 
-export const Loading = (): JSX.Element => <Box className="alignCenter"><FormattedMessage id="general.loading.text"/></Box>
+export const Loading: React.FC = (): JSX.Element => {
+  return (
+    <>
+      <Box className="alignCenter"><FormattedMessage id="general.loading.text"/><CircularProgress/></Box>
+    </>
+  );
+};
