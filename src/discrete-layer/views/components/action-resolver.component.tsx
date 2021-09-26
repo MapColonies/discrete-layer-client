63a9be3fd1fdafd96280577cd38a656d919f0b0a
Ella <ellamartirosyan@gmail.com>
2021-09-26 21:01:28 +0300

    fix: order from context menu

--- src/discrete-layer/views/components/action-resolver.component.tsx
+++ src/discrete-layer/views/components/action-resolver.component.tsx
@@ -44,22 +44,30 @@ export const ActionResolver: React.FC<ActionResolverComponentProps> = observer((
         case 'LayerRasterRecord.moveToTop':
           numOfLayers = (store.bestStore.layersList as LayerRasterRecordModelType[]).length - 1;
           order = store.bestStore.getLayerOrder(data.id as string);
-          store.bestStore.updateMovedLayer({ id: data.id, from: numOfLayers - order, to: 0 } as MovedLayer);
+          if (order !== numOfLayers) {
+            store.bestStore.updateMovedLayer({ id: data.id, from: numOfLayers - order, to: 0 } as MovedLayer);
+          }
           break;
         case 'LayerRasterRecord.moveUp':
           numOfLayers = (store.bestStore.layersList as LayerRasterRecordModelType[]).length - 1;
           order = store.bestStore.getLayerOrder(data.id as string);
-          store.bestStore.updateMovedLayer({ id: data.id, from: numOfLayers - order, to: numOfLayers - order - 1 } as MovedLayer);
+          if (order !== numOfLayers) {
+            store.bestStore.updateMovedLayer({ id: data.id, from: numOfLayers - order, to: numOfLayers - order - 1 } as MovedLayer);
+          }
           break
         case 'LayerRasterRecord.moveDown':
           numOfLayers = (store.bestStore.layersList as LayerRasterRecordModelType[]).length - 1;
           order = store.bestStore.getLayerOrder(data.id as string);
-          store.bestStore.updateMovedLayer({ id: data.id, from: numOfLayers - order, to: numOfLayers - order + 1 } as MovedLayer);
+          if (order !== 0) {
+            store.bestStore.updateMovedLayer({ id: data.id, from: numOfLayers - order, to: numOfLayers - order + 1 } as MovedLayer);
+          }
           break;
         case 'LayerRasterRecord.moveToBottom':
           numOfLayers = (store.bestStore.layersList as LayerRasterRecordModelType[]).length - 1;
           order = store.bestStore.getLayerOrder(data.id as string);
-          store.bestStore.updateMovedLayer({ id: data.id, from: numOfLayers - order, to: numOfLayers } as MovedLayer);
+          if (order !== 0) {
+            store.bestStore.updateMovedLayer({ id: data.id, from: numOfLayers - order, to: numOfLayers } as MovedLayer);
+          }
           break;
         default:
           break;
