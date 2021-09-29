481f8baab33c7d9bbf1bb27927b7ef8c244d9731
Alex Lebinson <newdarkvirus@gmail.com>
2021-09-29 10:50:01 +0300

    fix: main view rafactor (#160)

---
+++ src/discrete-layer/views/components/data-fetchers/static-data-fetcher.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { EntityDescriptorsFetcher } from './entity-descriptors-fetcher.component';
+
+
+export const StaticDataFetcher: React.FC = () => {
+  return (
+    <>
+      <EntityDescriptorsFetcher/>
+    </>
+  );
+};
