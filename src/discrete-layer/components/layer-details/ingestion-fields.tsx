566d515c054ac92a45973090a62e64c5ac7d93b3
Ella <ellamartirosyan@gmail.com>
2021-10-20 14:59:00 +0300

    fix: ingestion fields component

--- src/discrete-layer/components/layer-details/ingestion-fields.tsx
+++ src/discrete-layer/components/layer-details/ingestion-fields.tsx
@@ -1,48 +1,45 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
 import { Box } from '@map-colonies/react-components';
 import { Mode } from '../../../common/models/mode.enum';
 import { FieldLabelComponent } from '../../../common/components/form/field-label';
-import { RecordType } from '../../models';
 import { StringValuePresentorComponent } from './field-value-presentors/string.value-presentors';
 
 import './ingestion-fields.css';
 
+interface IFieldInfo {
+  fieldName: string;
+  label: string;
+  value: string;
+}
+
 interface IngestionFieldsProps {
-  recordType?: RecordType;
-  directory: string;
-  fileNames: string;
+  fields: IFieldInfo[];
   formik?: unknown;
 }
 
-export const IngestionFields: React.FC<IngestionFieldsProps> = (props: IngestionFieldsProps) => {
-  const { recordType, directory, fileNames, formik } = props;
-
+export const IngestionFields: React.FC<IngestionFieldsProps> = ({ fields, formik }) => {
   return (
     <Box className="ingestionFields">
       <Box className="categoryField">
-        <FieldLabelComponent value='field-names.ingestion.directory' isRequired={true} customClassName='directorySpacer'/>
-        <StringValuePresentorComponent 
-          mode={Mode.NEW} 
-          // @ts-ignore
-          fieldInfo={{
-            fieldName: 'directory',
-            isRequired: true
-          }} 
-          value={directory} 
-          formik={formik}>
-        </StringValuePresentorComponent>
-        <FieldLabelComponent value={recordType === RecordType.RECORD_3D ? 'field-names.3d.fileNames' : 'field-names.raster.fileNames'} isRequired={true} customClassName='fileNamesSpacer'/>
-        <StringValuePresentorComponent 
-          mode={Mode.NEW} 
-          // @ts-ignore
-          fieldInfo={{
-            fieldName: 'fileNames',
-            isRequired: true
-          }}
-          value={fileNames} 
-          formik={formik}>
-        </StringValuePresentorComponent>
+        {
+          fields.map(field => {
+            return (
+              <>
+                <FieldLabelComponent value={field.label} isRequired={true} customClassName={ `${field.fieldName}Spacer` }/>
+                <StringValuePresentorComponent 
+                  mode={Mode.NEW} 
+                  // @ts-ignore
+                  fieldInfo={{
+                    fieldName: field.fieldName,
+                    isRequired: true
+                  }} 
+                  value={field.value} 
+                  formik={formik}>
+                </StringValuePresentorComponent>
+              </>
+            );
+          })
+        }
       </Box>
     </Box>
   );
