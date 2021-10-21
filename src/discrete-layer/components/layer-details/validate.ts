5c75ae72019e4bb8406f11705b7f7e43b1ee6917
Ella <ellamartirosyan@gmail.com>
2021-10-21 22:25:25 +0300

    feat: vest validations

--- src/discrete-layer/components/layer-details/validate.ts
+++ src/discrete-layer/components/layer-details/validate.ts
@@ -1,19 +1,81 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import vest, { test, enforce } from 'vest';
+import { useIntl } from 'react-intl';
 // import { IVestResult } from 'vest/vestResult';
 
 const suite = vest.create((data: any = {}): any => {
+  const intl = useIntl();
 
-  test('directory', 'Directory is required', () => {
-    enforce(data.directory as string).isNotEmpty();
+  test('productName', intl.formatMessage({ id: 'validation-general.required' }), () => {
+    enforce(data['productName'] as string).isNotEmpty();
   });
 
-  test('fileNames', 'File name is required', () => {
-    enforce(data.fileNames as string).isNotEmpty();
+  test('creationDate', intl.formatMessage({ id: 'validation-general.required' }), () => {
+    enforce(data['creationDate'] as Date).isNotEmpty();
   });
 
-  test('version', 'Minimum of 2 required', () => {
-    enforce(data.version as number).greaterThan(1);
+  test('sourceDateStart', intl.formatMessage({ id: 'validation-field.sourceDateStart.max' }), () => {
+    enforce(data['sourceDateStart'] as Date).lessThan(data['sourceDateEnd']);
+  });
+
+  test('sourceDateEnd', intl.formatMessage({ id: 'validation-field.sourceDateEnd.min' }), () => {
+    enforce(data['sourceDateEnd'] as Date).greaterThanOrEquals(data['sourceDateStart']);
+  });
+
+  test('sensorType', intl.formatMessage({ id: 'validation-general.required' }), () => {
+    enforce(data['sensorType'] as string).isNotEmpty();
+  });
+
+  test('region', intl.formatMessage({ id: 'validation-general.required' }), () => {
+    enforce(data['region'] as string).isNotEmpty();
+  });
+
+  test('productId', intl.formatMessage({ id: 'validation-general.required' }), () => {
+    enforce(data['productId'] as string).isNotEmpty();
+  });
+
+  test('productVersion', intl.formatMessage({ id: 'validation-general.required' }), () => {
+    enforce(data['productVersion'] as string).isNotEmpty();
+  });
+
+  test('productVersion', intl.formatMessage({ id: 'validation-field.productVersion.pattern' }), () => {
+    enforce(data['productVersion'] as string).matches('^[1-9]\\d{0,2}(\\.(0|[1-9]\\d?))?$');
+  });
+
+  test('productType', intl.formatMessage({ id: 'validation-general.required' }), () => {
+    enforce(data['productType'] as string).isNotEmpty();
+  });
+
+  test('resolution', intl.formatMessage({ id: 'validation-general.required' }), () => {
+    enforce(data['resolution'] as number).isNotEmpty();
+  });
+
+  test('resolution', intl.formatMessage({ id: 'validation-field.resolution.min' }), () => {
+    enforce(data['resolution'] as number).greaterThanOrEquals(0.00000009);
+  });
+
+  test('resolution', intl.formatMessage({ id: 'validation-field.resolution.max' }), () => {
+    enforce(data['resolution'] as number).lessThan(0.072);
+  });
+
+  test('maxResolutionMeter', intl.formatMessage({ id: 'validation-general.required' }), () => {
+    enforce(data['maxResolutionMeter'] as number).isNotEmpty();
+  });
+
+  test('maxResolutionMeter', intl.formatMessage({ id: 'validation-field.maxResolutionMeter.min' }), () => {
+    enforce(data['maxResolutionMeter'] as number).greaterThanOrEquals(0.01);
+  });
+
+  test('maxResolutionMeter', intl.formatMessage({ id: 'validation-field.maxResolutionMeter.max' }), () => {
+    enforce(data['maxResolutionMeter'] as number).lessThan(8000);
+  });
+
+  test('scale', intl.formatMessage({ id: 'validation-field.scale.pattern' }), () => {
+    enforce(data['scale'] as string).matches('^(0|[1-9]\\d{0,8})$');
+  });
+
+  test('footprint', intl.formatMessage({ id: 'validation-general.required' }), () => {
+    enforce(data['footprint'] as string).isNotEmpty();
   });
 
 });
