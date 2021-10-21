f96af49476bed93aedbe61b48d0f6d54ba1738e2
Ella <ellamartirosyan@gmail.com>
2021-10-21 21:17:06 +0300

    feat: number presentor

--- src/discrete-layer/components/layer-details/field-value-presentors/number.value-presentors.tsx
+++ src/discrete-layer/components/layer-details/field-value-presentors/number.value-presentors.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import { get } from  'lodash';
-import { Icon, TextField, Tooltip } from '@map-colonies/react-core';
-import { Box } from '@map-colonies/react-components';
 import { Mode } from '../../../../common/models/mode.enum';
 import { IRecordFieldInfo } from '../layer-details.field-info';
-
-const EMPTY = 0;
+import { FormInputTextFieldComponent } from './form.input.text.field';
 
 interface NumberValuePresentorProps {
   mode: Mode;
@@ -15,50 +10,8 @@ interface NumberValuePresentorProps {
   formik?: unknown;
 }
 
-export const NumberValuePresentorComponent: React.FC<NumberValuePresentorProps> = ({ mode, fieldInfo, value, formik }) => {
-  if (formik === undefined || mode === Mode.VIEW || (mode === Mode.EDIT && fieldInfo.isManuallyEditable !== true)) {
-    return (
-      <Tooltip content={value}>
-        <Box className="detailsFieldValue">
-          {value}
-        </Box>
-      </Tooltip>
-    );
-  } else {
-    const value = get(formik,`values[${fieldInfo.fieldName as string}]`) as string;
-    return (
-      <Box className="detailsFieldValue">
-        <TextField
-          id={fieldInfo.fieldName as string}
-          name={fieldInfo.fieldName as string}
-          type="text"
-          // eslint-disable-next-line
-          onChange={(formik as any).handleChange}
-          // eslint-disable-next-line
-          onBlur={(formik as any).handleBlur}
-          value={value}
-          required={fieldInfo.isRequired === true}
-        />
-        {
-          fieldInfo.infoMsgCode && (fieldInfo.infoMsgCode as string[]).length > EMPTY &&
-          <>
-            {' '}
-            <Tooltip content={
-              <ul className="textFieldInfoList">
-                {
-                  (fieldInfo.infoMsgCode as string[]).map((msg: string, index: number) => {
-                    return (
-                      <li key={index}><FormattedMessage id={msg}/></li>
-                    );
-                  })
-                }
-              </ul>
-            }>
-              <Icon className="textFieldInfoIcon" icon={{ icon: 'info', size: 'small' }}/>
-            </Tooltip>
-          </>
-        }
-      </Box>
-    );
-  }
+export const NumberValuePresentorComponent: React.FC<NumberValuePresentorProps> = (props) => {
+  return (
+    <FormInputTextFieldComponent {...props} type="text"/>
+  );
 }
