1c851bdec2409fd448f43b1aee3a2a43561f7cb4
Ella <ellamartirosyan@gmail.com>
2021-10-21 21:12:29 +0300

    feat: refactor presentors

---
+++ src/discrete-layer/components/layer-details/field-value-presentors/form.input.text.field.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import { get } from  'lodash';
+import { Icon, TextField, Tooltip } from '@map-colonies/react-core';
+import { Box } from '@map-colonies/react-components';
+import { Mode } from '../../../../common/models/mode.enum';
+import { IRecordFieldInfo } from '../layer-details.field-info';
+
+const EMPTY = 0;
+
+interface FormInputTextFieldProps {
+  mode: Mode;
+  fieldInfo: IRecordFieldInfo;
+  value?: string;
+  formik?: unknown;
+  type?: string;
+}
+
+export const FormInputTextFieldComponent: React.FC<FormInputTextFieldProps> = ({ mode, fieldInfo, value, formik, type }) => {
+  if (formik === undefined || mode === Mode.VIEW || (mode === Mode.EDIT && fieldInfo.isManuallyEditable !== true)) {
+    return (
+      <Tooltip content={value}>
+        <Box className="detailsFieldValue">
+          {value}
+        </Box>
+      </Tooltip>
+    );
+  } else {
+    const value = get(formik,`values[${fieldInfo.fieldName as string}]`) as string;
+    return (
+      <Box className="detailsFieldValue">
+        <TextField
+          id={fieldInfo.fieldName as string}
+          name={fieldInfo.fieldName as string}
+          type={type}
+          // eslint-disable-next-line
+          onChange={(formik as any).handleChange}
+          // eslint-disable-next-line
+          onBlur={(formik as any).handleBlur}
+          value={value}
+          required={fieldInfo.isRequired === true}
+        />
+        {
+          fieldInfo.infoMsgCode && (fieldInfo.infoMsgCode as string[]).length > EMPTY &&
+          <>
+            {' '}
+            <Tooltip content={
+              <ul className="textFieldInfoList">
+                {
+                  (fieldInfo.infoMsgCode as string[]).map((msg: string, index: number) => {
+                    return (
+                      <li key={index}><FormattedMessage id={msg}/></li>
+                    );
+                  })
+                }
+              </ul>
+            }>
+              <Icon className="textFieldInfoIcon" icon={{ icon: 'info', size: 'small' }}/>
+            </Tooltip>
+          </>
+        }
+      </Box>
+    );
+  }
+}
--- src/discrete-layer/components/layer-details/field-value-presentors/string.value-presentors.tsx
+++ src/discrete-layer/components/layer-details/field-value-presentors/string.value-presentors.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import { get } from  'lodash';
-import { Icon, TextField, Tooltip } from '@map-colonies/react-core';
-import { Box } from '@map-colonies/react-components';
 import { Mode } from '../../../../common/models/mode.enum';
 import { IRecordFieldInfo } from '../layer-details.field-info';
-
-const EMPTY = 0;
+import { FormInputTextFieldComponent } from './form.input.text.field';
 
 interface StringValuePresentorProps {
   mode: Mode;
@@ -15,50 +10,8 @@ interface StringValuePresentorProps {
   formik?: unknown;
 }
 
-export const StringValuePresentorComponent: React.FC<StringValuePresentorProps> = ({ mode, fieldInfo, value, formik }) => {
-  if (formik === undefined || mode === Mode.VIEW || (mode === Mode.EDIT && fieldInfo.isManuallyEditable !== true)) {
-    return (
-      <Tooltip content={value}>
-        <Box className="detailsFieldValue">
-          {value}
-        </Box>
-      </Tooltip>
-    );
-  } else {
-    const value = get(formik,`values[${fieldInfo.fieldName as string}]`) as string;
-    return (
-      <Box className="detailsFieldValue">
-        <TextField
-          id={fieldInfo.fieldName as string}
-          name={fieldInfo.fieldName as string}
-          type="text"
-          // eslint-disable-next-line
-          onChange={(formik as any).handleChange}
-          // eslint-disable-next-line
-          onBlur={(formik as any).handleBlur}
-          value={value}
-          required={fieldInfo.isRequired === true}
-        />
-        {
-          fieldInfo.infoMsgCode && (fieldInfo.infoMsgCode as string[]).length > EMPTY &&
-          <>
-            {' '}
-            <Tooltip content={
-              <ul style={{ listStyle: 'none', margin: 0, padding: 0 }}>
-                {
-                  (fieldInfo.infoMsgCode as string[]).map((msg: string, index: number) => {
-                    return (
-                      <li key={index}><FormattedMessage id={msg}/></li>
-                    );
-                  })
-                }
-              </ul>
-            }>
-              <Icon className="textFieldInfoIcon" icon={{ icon: 'info', size: 'small' }}/>
-            </Tooltip>
-          </>
-        }
-      </Box>
-    );
-  }
+export const StringValuePresentorComponent: React.FC<StringValuePresentorProps> = (props) => {
+  return (
+    <FormInputTextFieldComponent {...props} type="text"/>
+  );
 }
