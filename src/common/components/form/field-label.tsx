268735fe836b922e297e2a9fc2e0a4bfbd6a5127
Ella <ellamartirosyan@gmail.com>
2021-10-28 10:14:42 +0300

    fix: use class name instead of style

--- src/common/components/form/field-label.tsx
+++ src/common/components/form/field-label.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Box } from '@map-colonies/react-components';
 
+import './field-label.css';
+
 interface FieldLabelProps {
   value?: string;
   isRequired?: boolean;
@@ -14,7 +16,7 @@ export const FieldLabelComponent: React.FC<FieldLabelProps> = ({value, isRequire
       <Box className={customClassName !== undefined ? customClassName : 'detailsFieldLabel'}>
         <FormattedMessage id={value}/>
       </Box>
-      <Box style={{ flex: '0 0 8px', color: 'var(--mdc-theme-gc-error-medium)' }}>{isRequired === true ? '*' : ''}</Box>
+      <Box className="requiredAsterisk">{isRequired === true ? '*' : ''}</Box>
     </>
   );
 }
