f05d5a658d68916b16a4a747a92ad5ac238907b4
Alex Lebinson <newdarkvirus@gmail.com>
2021-11-04 17:41:47 +0200

    Input label visual bug (#180)
    * Info icon fix
    
    * FIx: Broken input
    
    * Fixed required sign placement + tooltip on ellipsis
    
    * fix(field-label): added usememo for label rendering
    
    Co-authored-by: Shlomi Koncha <cl_shlomikoncha@rnd-hub.com>

--- src/common/components/form/field-label.tsx
+++ src/common/components/form/field-label.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import React, { useMemo } from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
+import { Tooltip } from '@map-colonies/react-core';
 import { Box } from '@map-colonies/react-components';
 
 import './field-label.css';
@@ -10,13 +11,44 @@ interface FieldLabelProps {
   customClassName?: string;
 }
 
-export const FieldLabelComponent: React.FC<FieldLabelProps> = ({value, isRequired, customClassName}) => {
+export const FieldLabelComponent: React.FC<FieldLabelProps> = ({
+  value,
+  isRequired,
+  customClassName,
+}) => {
+  const intl = useIntl();
+
+  const renderLabel = useMemo(() => {
+    // Indicates when the label will overflow the container
+    const MAX_LABEL_LENGTH = 12;
+    const formattedLabel = intl.formatMessage({ id: value });
+
+    if (formattedLabel.length > MAX_LABEL_LENGTH) {
+      return (
+        <Tooltip content={formattedLabel}>
+          <span>
+            <FormattedMessage id={value} />
+          </span>
+        </Tooltip>
+      );
+    }
+    return (
+      <span>
+        <FormattedMessage id={value} />
+      </span>
+    );
+  },[]);
+
   return (
     <>
-      <Box className={customClassName !== undefined ? customClassName : 'detailsFieldLabel'}>
-        <FormattedMessage id={value}/>
+      <Box
+        className={
+          customClassName !== undefined ? customClassName : 'detailsFieldLabel'
+        }
+      >
+        {renderLabel}
+        <Box className="requiredAsterisk">{isRequired === true ? '*' : ''}</Box>
       </Box>
-      <Box className="requiredAsterisk">{isRequired === true ? '*' : ''}</Box>
     </>
   );
-}
+};
