e0926e5c4e965af3bfd3554683fa21ad7f6eb8c5
Ella <ellamartirosyan@gmail.com>
2021-11-10 15:59:41 +0200

    fix: if

--- src/discrete-layer/components/layer-details/field-value-presentors/form.input.info.tooltip.tsx
+++ src/discrete-layer/components/layer-details/field-value-presentors/form.input.info.tooltip.tsx
@@ -20,25 +20,28 @@ export const FormInputInfoTooltipComponent: React.FC<FormInputInfoTooltipProps> 
   const getInfoMsg = (fieldInfo: IRecordFieldInfo, msgCode: string): string => {
     let infoMsgParamValue = '';
     const infoMsgType = getInfoMsgValidationType(msgCode);
-    const validation = fieldInfo.validation !== undefined ? fieldInfo.validation as ValidationConfigModelType[] : undefined;
-    validation?.forEach((val: ValidationConfigModelType) => {
-      const validationType = getValidationType(val) ?? '';
-      if (validationType === infoMsgType) {
-        // @ts-ignore
-        // eslint-disable-next-line
-        const validationParamValue: string = val[validationType] ?? '';
-        if (validationParamValue !== '') {
-          if (val.type === ValidationType.FIELD) {
-            const fieldLabel = fieldInfo.label as string;
-            const fieldLabelPrefix = fieldLabel.substring(START, fieldLabel.lastIndexOf('.'));
-            infoMsgParamValue = intl.formatMessage({ id: `${fieldLabelPrefix}.${validationParamValue}` });
-          } else {
-            infoMsgParamValue = convertExponentialToDecimal(validationParamValue);
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    if (infoMsgType !== undefined) {
+      const validation = fieldInfo.validation !== undefined ? fieldInfo.validation as ValidationConfigModelType[] : undefined;
+      validation?.forEach((val: ValidationConfigModelType) => {
+        const validationType = getValidationType(val) ?? '';
+        if (validationType === infoMsgType) {
+          // @ts-ignore
+          // eslint-disable-next-line
+          const validationParamValue: string = val[validationType] ?? '';
+          if (validationParamValue !== '') {
+            if (val.type === ValidationType.FIELD) {
+              const fieldLabel = fieldInfo.label as string;
+              const fieldLabelPrefix = fieldLabel.substring(START, fieldLabel.lastIndexOf('.'));
+              infoMsgParamValue = intl.formatMessage({ id: `${fieldLabelPrefix}.${validationParamValue}` });
+            } else {
+              infoMsgParamValue = convertExponentialToDecimal(validationParamValue);
+            }
+            return;
           }
-          return;
         }
-      }
-    });
+      });
+    }
     return intl.formatMessage({ id: msgCode }, { value: `<strong>${infoMsgParamValue}</strong>` });
   };
 
