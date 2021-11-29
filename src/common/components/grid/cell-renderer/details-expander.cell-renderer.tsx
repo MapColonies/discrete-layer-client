ba7338bfc5d0a949bb44acb31e151ea1227bba3d
Assaf Chamoy <assafchamoy97@gmail.com>
2021-11-29 15:55:38 +0200

    feat: rotate expander icon when details are visible

--- src/common/components/grid/cell-renderer/details-expander.cell-renderer.tsx
+++ src/common/components/grid/cell-renderer/details-expander.cell-renderer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ICellRendererParams } from 'ag-grid-community';
 import { Icon } from '@map-colonies/react-core';
 import { Box } from '@map-colonies/react-components';
@@ -7,21 +7,35 @@ import { DETAILS_ROW_ID_SUFFIX, IGridRowDataDetailsExt } from '../grid';
 import './details-expander.cell-renderer.css';
 
 export const DetailsExpanderRenderer: React.FC<ICellRendererParams> = (props) => {
-  const value: boolean = (props.data as IGridRowDataDetailsExt).isVisible;
+
+  const BASE_ICON_ANGLE = 0;
+  const UPSIDE_DOWN_ANGLE = 180;
+
+  const [visible, setVisible] = useState(false);
+  const [iconRotation, setIconRotation] = useState(BASE_ICON_ANGLE);
   
   const handleCollapseExpand = (): void => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    const rowNode = props.api.getRowNode(`${props.data?.id as string}${DETAILS_ROW_ID_SUFFIX}`);
-    rowNode.setDataValue('isVisible', !(rowNode.data as IGridRowDataDetailsExt).isVisible);
+    const rowNode = props.api.getRowNode(`${props.data?.id as string}${DETAILS_ROW_ID_SUFFIX}`);   
+    const isVisible = (rowNode.data as IGridRowDataDetailsExt).isVisible;
+    setVisible(!isVisible);
+    rowNode.setDataValue('isVisible', !isVisible);
+
     props.api.onFilterChanged();
   };
+
+  // Rotating the icon when the details container is visible.
+  useEffect(()=>{
+    setIconRotation(visible ? UPSIDE_DOWN_ANGLE : BASE_ICON_ANGLE );
+  }, [visible])
 
   return (
     <>
       {
-      value && <Box className="expanderContainer" onClick={handleCollapseExpand}>
+      <Box className="expanderContainer" onClick={handleCollapseExpand}>
         <Icon 
-          style={{ color: 'var(--mdc-theme-primary)' }}
+          className={'expanderIcon'}
+          style={{transform: `rotate(${iconRotation}deg)`}}
           icon={{ icon: 'expand_more', size: 'xsmall' }}
         />
       </Box>
