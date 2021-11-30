8ab5cff71049583bb3a4a77b6f0d104d91413c0a
Assaf Chamoy <assafchamoy97@gmail.com>
2021-11-30 18:29:49 +0200

    feat: making of collapse button component for later use

--- src/common/components/grid/cell-renderer/details-expander.cell-renderer.tsx
+++ src/common/components/grid/cell-renderer/details-expander.cell-renderer.tsx
@@ -1,46 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { ICellRendererParams } from 'ag-grid-community';
-import { Icon } from '@map-colonies/react-core';
-import { Box } from '@map-colonies/react-components';
 import { DETAILS_ROW_ID_SUFFIX, IGridRowDataDetailsExt } from '../grid';
+import {CollapseButton} from '../../collapse-button/collapse.button';
 
 import './details-expander.cell-renderer.css';
 
 export const DetailsExpanderRenderer: React.FC<ICellRendererParams> = (props) => {
 
-  const BASE_ICON_ANGLE = 0;
-  const UPSIDE_DOWN_ANGLE = 180;
-
-  const [visible, setVisible] = useState(false);
-  const [iconRotation, setIconRotation] = useState(BASE_ICON_ANGLE);
-  
   const handleCollapseExpand = (): void => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     const rowNode = props.api.getRowNode(`${props.data?.id as string}${DETAILS_ROW_ID_SUFFIX}`);   
     const isVisible = (rowNode.data as IGridRowDataDetailsExt).isVisible;
-    setVisible(!isVisible);
     rowNode.setDataValue('isVisible', !isVisible);
-
     props.api.onFilterChanged();
   };
 
-  // Rotating the icon when the details container is visible.
-  useEffect(()=>{
-    setIconRotation(visible ? UPSIDE_DOWN_ANGLE : BASE_ICON_ANGLE );
-  }, [visible])
-
-  return (
-    <>
-      {
-      <Box className="expanderContainer" onClick={handleCollapseExpand}>
-        <Icon 
-          className={'expanderIcon'}
-          style={{transform: `rotate(${iconRotation}deg)`}}
-          icon={{ icon: 'expand_more', size: 'xsmall' }}
-        />
-      </Box>
-      }
-    </>
-  );
+  return <CollapseButton onClick={handleCollapseExpand} />
 
 };
