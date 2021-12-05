7a4e5e9e31a0487abc89982f88f7f6fc6c2d6357
alebinson <newdarkvirus@gmail.com>
2021-12-05 12:46:30 +0200

    fix: models first generation

---
+++ src/discrete-layer/models/LayerDemRecordModel.ts
@@ -0,0 +1,33 @@
+import { Instance, types } from "mobx-state-tree"
+import { LayerDemRecordModelBase } from "./LayerDemRecordModel.base"
+import { momentDateType } from "./moment-date.type"
+
+/* The TypeScript type of an instance of LayerDemRecordModel */
+export interface LayerDemRecordModelType extends Instance<typeof LayerDemRecordModel.Type> {}
+
+/* A graphql query fragment builders for LayerDemRecordModel */
+export { selectFromLayerDemRecord, layerDemRecordModelPrimitives, LayerDemRecordModelSelector } from "./LayerDemRecordModel.base"
+
+/**
+ * LayerDemRecordModel
+ */
+export const LayerDemRecordModel = LayerDemRecordModelBase
+  .actions(self => ({
+    // This is an auto-generated example action.
+    log() {
+      console.log(JSON.stringify(self))
+    }
+  }))
+  .props({
+    /* eslint-disable */
+    /* tslint:disable */
+    insertDate: types.maybe(momentDateType),
+    updateDate: types.maybe(momentDateType),
+    sourceDateStart: types.maybe(momentDateType),
+    sourceDateEnd: types.maybe(momentDateType),
+    /* tslint:enable */
+    /* eslint-enable */
+    footprintShown: types.union(types.undefined, types.null, types.boolean),
+    order: types.union(types.undefined, types.null, types.number),
+    layerImageShown: types.union(types.undefined, types.null, types.boolean),
+  })
