adfb5b3c53ce295824249e3cc96dff0c18ad3e89
Assaf Chamoy <assafchamoy97@gmail.com>
2021-12-06 18:08:07 +0200

    feat: changed status component according to the new requirements

--- src/discrete-layer/components/system-status/cell-renderer/status.cell-renderer.tsx
+++ src/discrete-layer/components/system-status/cell-renderer/status.cell-renderer.tsx
@@ -1,37 +1,98 @@
 import React from 'react';
 import { ICellRendererParams } from 'ag-grid-community';
 import { Box } from '@map-colonies/react-components';
-import { Tooltip } from '@map-colonies/react-core';
-import { JobModelType, Status, TaskModelType } from '../../../models';
+import { Typography } from '@map-colonies/react-core';
+import { JobModelType } from '../../../models';
 
 import './status.cell-renderer.css';
+import { useIntl } from 'react-intl';
 
 const NO_DATA = 0;
+const NO_WIDTH = 0;
+
+const STATUS_BAR_WIDTH = 190;
+const STATUS_BAR_HEIGHT = 18;
 
 export const StatusRenderer: React.FC<ICellRendererParams> = (props) => {
-  const getProgress = (data: JobModelType): string => {
-    if(!data.tasks || data.tasks.length === NO_DATA){
-      return '';
-    }
-    const completed = data.tasks.filter((task: TaskModelType)=> task.status === Status.Completed);
-    return `(${completed.length}/${data.tasks.length})`;
+  const intl = useIntl();
+  const jobData = props.data as JobModelType;
+  const status = jobData.status;
+
+  const getProgress = (): string => {
+    const finalStatusCount: number =
+      (jobData.failedTasks as number) + (jobData.completedTasks as number);
+    return `(${finalStatusCount}/${jobData.taskCount as number})`;
   };
 
   const getProgressComponent = (): JSX.Element => {
+
+    const statusText = intl.formatMessage({ id: `system-status.job.status_translation.${status as string}`});
+
+
     return (
-      <Box className={`${status?.toLowerCase() as string}`}>
-        {status}&nbsp;&nbsp;{getProgress(jobData)}
+      <Box>
+        {`${statusText}  ${getProgress()}`}
+        {/* {statusText}&nbsp;&nbsp;{getProgress()} */}
       </Box>
     );
-  }
-  const jobData = props.data as JobModelType;
-  const status = jobData.status;
+  };
+
+  type StatusCountType = number | null | undefined;
+
+  const calcStatusWidth = (statusCount: StatusCountType): number => {
+    const { taskCount } = jobData;
+    return (taskCount as number) > NO_DATA
+      ? ((statusCount as number) / (taskCount as number)) * STATUS_BAR_WIDTH
+      : NO_WIDTH;
+  };
+
+  const getProgressbarSections = () => {
+    const { completedTasks, inProgressTasks, failedTasks } = jobData;
+    const completedWidth = calcStatusWidth(completedTasks);
+    const failedWidth = calcStatusWidth(failedTasks);
+    const inProgressWidth = calcStatusWidth(inProgressTasks);
+
+    return (
+      <Box className={'progressSectionsContainer'}>
+        <Box
+          className={'completedArea'}
+          style={{
+            width: `${completedWidth}px`,
+            height: `${STATUS_BAR_HEIGHT}px`,
+          }}
+        />
+        <Box
+          className={'failedArea'}
+          style={{
+            width: `${failedWidth}px`,
+            height: `${STATUS_BAR_HEIGHT}px`,
+          }}
+        />
+        <Box
+          className={'inProgressArea'}
+          style={{
+            width: `${inProgressWidth}px`,
+            height: `${STATUS_BAR_HEIGHT}px`,
+          }}
+        />
+      </Box>
+    );
+  };
+
   return (
-    status === 'Failed' ? 
-      <Tooltip content={jobData.reason}>
+    <Box className={'statusBarContainer'}>
+      <Typography style={{fontSize: '12px'}} tag="p" className="statusText">
         {getProgressComponent()}
-      </Tooltip>
-    :
-      <>{getProgressComponent()}</>
- );
+      </Typography>
+      <Box
+        className={'statusBar'}
+        style={{
+          width: `${STATUS_BAR_WIDTH}px`,
+          height: `${STATUS_BAR_HEIGHT}px`,
+        }}
+      >
+        {getProgressbarSections()}
+      </Box>
+    </Box>
+  );
 };
