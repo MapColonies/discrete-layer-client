af74620f067cf0a92aa6688700c463d8a404fbbe
Ella <ellamartirosyan@gmail.com>
2021-12-13 18:15:55 +0200

    fix: entity type duplication

---
+++ src/common/components/general/entity-type.icon.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import React from 'react';
+import { IconButton, Tooltip } from '@map-colonies/react-core';
+import { Box } from '@map-colonies/react-components';
+import { ILayerImage } from '../../../discrete-layer/models/layerImage';
+import { LayerRasterRecordModelType } from '../../../discrete-layer/models';
+
+interface IEntityTypeProps {
+  data: ILayerImage;
+  style?: Record<string, unknown>;
+}
+
+export const EntityTypeIcon: React.FC<IEntityTypeProps> = ({ data, style }) => {
+
+  const type = data.__typename;
+
+  interface EntityTypeIconsAndTooltips {
+    [key: string]: [string, string] | undefined;
+  }
+
+  let includedInBests;
+  if (type === 'LayerRasterRecord') {
+    includedInBests = (data as LayerRasterRecordModelType).includedInBests;
+  }
+
+  const entityTypeIconsAndTooltips: EntityTypeIconsAndTooltips = {
+    'LayerRasterRecord': [
+      includedInBests ? 'mc-icon-Map-Best-Orthophoto' : 'mc-icon-Map-Orthophoto',
+      `Orthophoto ${includedInBests ? (' included in: ' + includedInBests.join(' , ')) : ''}`
+    ],
+    'LayerDRecord': ['mc-icon-Map-3D', '3D'],
+    'BestRecord': ['mc-icon-Bests', 'BEST'],
+    'LayerDEMRecord': ['mc-icon-Map-Terrain', 'DEM'],
+  };
+  
+  const [icon, tooltip] = entityTypeIconsAndTooltips[type] ?? ['mc-icon-Close glow-missing-icon', 'MISSING ICON'];
+
+  return (
+    <Box style={style}>
+      <Tooltip content={tooltip}>
+        <IconButton className={icon} label="ENTITY TYPE ICON"/>
+      </Tooltip>
+    </Box>
+  );
+};
--- src/common/components/grid/cell-renderer/entity-type.cell-renderer.tsx
+++ src/common/components/grid/cell-renderer/entity-type.cell-renderer.tsx
@@ -1,46 +1,14 @@
 import React from 'react';
 import { ICellRendererParams } from 'ag-grid-community';
-import { IconButton, Tooltip } from '@map-colonies/react-core';
-import { Box } from '@map-colonies/react-components';
 import { ILayerImage } from '../../../../discrete-layer/models/layerImage';
-import { LayerRasterRecordModelType } from '../../../../discrete-layer/models';
+import { EntityTypeIcon } from '../../general/entity-type.icon';
 
 interface IEntityTypeCellRendererParams extends ICellRendererParams {
   style?: Record<string, unknown>;
 }
 
 export const EntityTypeRenderer: React.FC<IEntityTypeCellRendererParams> = (props) => {
-  const type = (props.data as ILayerImage).__typename;
-  let icon: string;
-  let includedInBests;
-  let value = '';
-  switch (type) {
-    case 'LayerRasterRecord':
-      includedInBests = (props.data as LayerRasterRecordModelType).includedInBests;
-      icon = includedInBests ? 'mc-icon-Map-Best-Orthophoto' : 'mc-icon-Map-Orthophoto';
-      value = `Orthophoto ${includedInBests ? (' included in: ' + includedInBests.join(' , ')) : ''}`;
-      break;
-    case 'Layer3DRecord':
-      icon = 'mc-icon-Map-3D';
-      value = '3D';
-      break;
-    case 'BestRecord':
-      icon = 'mc-icon-Bests';
-      value = 'BEST';
-      break;
-    case 'LayerDEMRecord':
-      icon = 'mc-icon-Map-Terrain';
-      value = 'DEM';
-      break;
-    default:
-      icon = '';
-  }
-
   return (
-    <Box style={props.style}>
-      <Tooltip content={value}>
-        <IconButton className={icon} label="ENTITY TYPE ICON"/>
-      </Tooltip>
-    </Box>
+    <EntityTypeIcon data={props.data as ILayerImage} style={props.style}/>
   );
 };
