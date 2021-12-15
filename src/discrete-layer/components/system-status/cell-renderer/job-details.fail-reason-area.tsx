424bc768aaae013f6a9b1a03a88cdb31599df2f9
Assaf Chamoy <assafchamoy97@gmail.com>
2021-12-15 16:26:58 +0200

    system jobs improvments (#217)
    * feat: job details - added time to creation and update dates
    
    * feat: job details header boilerplate
    
    * fix: jobs dialog - styling improvements 1
    
    * feat: rotate expander icon when details are visible
    
    * feat: making of collapse button component for later use
    
    * feat: fail reason component for job failed tasks
    
    * feat: start implementing collapsible task table rows on status fail
    
    * feat: collapsible task table rows on status fail styling fix
    
    * fix: job manager overflow behaviors (styles)
    
    * feat: added mock jobs data
    
    * fix: bug with date formatter - toISOString is not in date
    
    * fix: job row hover applied to job details container
    
    * feat: added job producer type icon to the grid
    
    * chore: update job model base with new fields and types
    
    * fix: job dialog and details styling for the new requirements
    
    * feat: changed status component according to the new requirements
    
    * feat: product type renderer (icon) and enum fix
    
    * feat: job details header component
    
    * chore: change job details cell renderer from table to grid
    
    * feat: status cell renderer new design improvements
    
    * fix: move status col to the end of the jobs data by new request
    
    * fix: job details header design fix
    
    * fix: reduce status bar length
    
    * fix: system jobs finishes
    
    * fix: no unused vars
    
    * fix: details expander cell renderer tests
    
    * fix: job-details header row instead of col
    
    * fix: pr fixes - added generic copy button component and more consts

---
+++ src/discrete-layer/components/system-status/cell-renderer/job-details.fail-reason-area.tsx
@@ -0,0 +1,44 @@
+import React,{useState, useEffect} from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard';
+import { useIntl } from 'react-intl';
+import { IconButton, Tooltip, Typography } from '@map-colonies/react-core';
+import { Box } from '@map-colonies/react-components';
+
+import './job-details.fail-reason-area.css';
+
+interface FailReasonAreaProps {
+  failReason: string;
+  show: boolean;
+  key?: string;
+}
+
+export const FailReasonArea: React.FC<FailReasonAreaProps> = ({
+  failReason,
+  show,
+  key = '',
+}) => {
+  const intl = useIntl();
+
+  const [containerClass, setContainerClass] = useState('failReasonAreaContainer');
+
+  useEffect(() => {
+    setContainerClass(`failReasonAreaContainer ${show ? 'show' : ''}`);
+  }, [show])
+
+
+  return (
+    <td colSpan={5}>
+      <Box key={key} className={containerClass}>
+        <Typography className={'failReasonText'} tag={'p'}>
+          {failReason}
+        </Typography>
+
+        <Tooltip content={intl.formatMessage({ id: 'action.copy.tooltip' })}>
+          <CopyToClipboard text={failReason}>
+            <IconButton className="mc-icon-Copy" />
+          </CopyToClipboard>
+        </Tooltip>
+      </Box>
+    </td>
+  );
+};
