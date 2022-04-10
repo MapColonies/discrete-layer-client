5f91dd834e63f27e5b12b5692d72b04dc2c6721a
assafchamoy <assafchamoy97@gmail.com>
2022-04-10 14:06:56 +0300

    chore: mc-models new fields implementation #2

--- src/discrete-layer/models/Layer3DRecordModel.base.ts
+++ src/discrete-layer/models/Layer3DRecordModel.base.ts
@@ -21,7 +21,7 @@ export const Layer3DRecordModelBase = ModelBase
   .props({
     __typename: types.optional(types.literal("Layer3DRecord"), "Layer3DRecord"),
     type: types.union(types.undefined, types.null, RecordTypeEnumType),
-    productId: types.union(types.undefined, types.null, types.string),
+    productId: types.union(types.undefined, types.string),
     productName: types.union(types.undefined, types.string),
     productVersion: types.union(types.undefined, types.null, types.string),
     productType: types.union(types.undefined, ProductTypeEnumType),
