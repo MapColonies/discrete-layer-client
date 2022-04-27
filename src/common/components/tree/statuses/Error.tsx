d693fa8c4c60e50b2bc6c4ea917692cef576e1db
Ella <ellamartirosyan@gmail.com>
2022-04-27 09:37:57 +0300

    fix: fix

--- src/common/components/tree/statuses/Error.tsx
+++ src/common/components/tree/statuses/Error.tsx
@@ -7,16 +7,14 @@ const START = 0;
 
 export const Error: React.FC = (props) => {
   const formatMessage = (message: string): string => {
-    return message.substr(START, +message.indexOf(': '));
+    return message.slice(START, +message.indexOf(': '));
   };
 
   return (
-    <>
-      <Box className="alignCenter failed">
-        <Box className="errorTitle"><Typography use="headline6" tag="div"><FormattedMessage id="general.error.text"/></Typography></Box>
-        <Box className="errorMessage"><Typography use="body2" tag="div"><FormattedMessage id="general.error.title"/></Typography></Box>
-        <Box className="errorDescription"><Typography use="body2" tag="div"><FormattedMessage id="general.error.description"/> {formatMessage(props.children as string)}</Typography></Box>
-      </Box>
-    </>
+    <Box className="alignCenter failed">
+      <Box className="errorTitle"><Typography use="headline6" tag="div"><FormattedMessage id="general.error.text"/></Typography></Box>
+      <Box className="errorMessage"><Typography use="body2" tag="div"><FormattedMessage id="general.error.title"/></Typography></Box>
+      <Box className="errorDescription"><Typography use="body2" tag="div"><FormattedMessage id="general.error.description"/> {formatMessage(props.children as string)}</Typography></Box>
+    </Box>
   );
 };
