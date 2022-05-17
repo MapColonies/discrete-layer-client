d24a31f43fbe52717c9df83184e315a5339a9ad8
Ella <ellamartirosyan@gmail.com>
2022-05-17 10:36:12 +0300

    fix: build

--- src/discrete-layer/models/QuantizedMeshBestRecordModel.ts
+++ src/discrete-layer/models/QuantizedMeshBestRecordModel.ts
@@ -1,4 +1,5 @@
-import { Instance } from "mobx-state-tree"
+import { Instance, types } from "mobx-state-tree"
+import { momentDateType } from "./moment-date.type"
 import { QuantizedMeshBestRecordModelBase } from "./QuantizedMeshBestRecordModel.base"
 
 /* The TypeScript type of an instance of QuantizedMeshBestRecordModel */
@@ -10,6 +11,7 @@ export { selectFromQuantizedMeshBestRecord, quantizedMeshBestRecordModelPrimitiv
 /**
  * QuantizedMeshBestRecordModel
  */
+// eslint-disable-next-line @typescript-eslint/naming-convention
 export const QuantizedMeshBestRecordModel = QuantizedMeshBestRecordModelBase
   .actions(self => ({
     // This is an auto-generated example action.
@@ -17,3 +19,18 @@ export const QuantizedMeshBestRecordModel = QuantizedMeshBestRecordModelBase
       console.log(JSON.stringify(self))
     }
   }))
+  .props({
+    /* eslint-disable */
+    /* tslint:disable */
+    insertDate: types.maybe(momentDateType),
+    creationDate: types.maybe(momentDateType),
+    updateDate: types.maybe(momentDateType),
+    ingestionDate: types.maybe(momentDateType),
+    /* tslint:enable */
+    /* eslint-enable */
+
+    // TODO: should be removed
+    footprintShown: types.union(types.undefined, types.null, types.boolean),
+    order: types.union(types.undefined, types.null, types.number),
+    layerImageShown: types.union(types.undefined, types.null, types.boolean),
+  })
