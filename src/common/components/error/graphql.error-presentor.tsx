01da4598f270bceb6b6c19de06fbad8bbfa7d763
EllaMartirosyan <34953077+EllaMartirosyan@users.noreply.github.com>
2022-09-04 18:30:15 +0300

    fix: timeout errors (#348)

--- src/common/components/error/graphql.error-presentor.tsx
+++ src/common/components/error/graphql.error-presentor.tsx
@@ -2,10 +2,13 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
 import { IconButton } from '@map-colonies/react-core';
 import { Box } from '@map-colonies/react-components';
 
 import './error-presentor.css';
+
+const SERVER_ERROR_RESPONSE_CODE = 500;
 
 interface IGpaphQLError {
   error: any;
@@ -24,11 +27,15 @@ export const GraphQLError: React.FC<IGpaphQLError> = (props)=> {
           <IconButton className="errorIcon mc-icon-Status-Warnings" />
           <ul className="errorsList">
             {
-              props.error.response.errors.map((error: Record<string, any>, index: number) => {
+              props.error.response.errors?.map((error: Record<string, any>, index: number) => {
                 return (
                   <li key={index}>{formatMessage(error.message)}</li>
                 );
               })
+            }
+            {
+              props.error.response.status >= SERVER_ERROR_RESPONSE_CODE &&
+              <li key={props.error.response.status as number}><FormattedMessage id="general.server.error"/></li>
             }
           </ul>
         </Box>
