37746328c53eb23d9fd601ef6b0be50dfca120e9
Ella <ellamartirosyan@gmail.com>
2022-09-11 19:08:45 +0300

    fix: unpublished layer icon

--- src/common/components/grid/cell-renderer/product-type.cell-renderer.tsx
+++ src/common/components/grid/cell-renderer/product-type.cell-renderer.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { ICellRendererParams } from 'ag-grid-community';
 import { getLinkUrlWithToken } from '../../../../discrete-layer/components/helpers/layersUtils';
+import { RecordStatus } from '../../../../discrete-layer/models';
 import { ILayerImage } from '../../../../discrete-layer/models/layerImage';
 import { LinkType } from '../../../models/link-type.enum';
 import { TypeIcon } from '../../general/type-icon';
+
+const STATUS = 'productStatus';
+const UNPUBLISHED_COLOR = 'orange';
 
 interface IEntityTypeCellRendererParams extends ICellRendererParams {
   style?: Record<string, unknown>;
@@ -12,12 +16,16 @@ interface IEntityTypeCellRendererParams extends ICellRendererParams {
 export const ProductTypeRenderer: React.FC<IEntityTypeCellRendererParams> = (props) => {
 
   const data = props.data as ILayerImage;
+  let style = {...props.style ?? {}};
+  if (STATUS in data && Object.values(data).includes(RecordStatus.UNPUBLISHED)) {
+    style = { ...style, color: UNPUBLISHED_COLOR };
+  }
 
   return (
     <TypeIcon
       typeName={data.productType as string}
       thumbnailUrl={data.links ? getLinkUrlWithToken(data.links, LinkType.THUMBNAIL_S) : undefined}
-      style={props.style}
+      style={style}
     />
   );
   
--- src/common/components/tree/icon-renderers/product-type.icon-renderer.tsx
+++ src/common/components/tree/icon-renderers/product-type.icon-renderer.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { RecordStatus } from '../../../../discrete-layer/models';
 import { ILayerImage } from '../../../../discrete-layer/models/layerImage';
 import { TypeIcon } from '../../general/type-icon';
+
+const STATUS = 'productStatus';
+const UNPUBLISHED_COLOR = 'orange';
 
 interface ILayerImageCellRendererParams {
   data: ILayerImage;
@@ -10,7 +14,11 @@ interface ILayerImageCellRendererParams {
 export const ProductTypeRenderer: React.FC<ILayerImageCellRendererParams> = ({ data, thumbnailUrl }) => {
 
   return (
-    <TypeIcon typeName={data.productType as string} thumbnailUrl={thumbnailUrl}/>
+    <TypeIcon
+      typeName={data.productType as string}
+      thumbnailUrl={thumbnailUrl}
+      style={STATUS in data && Object.values(data).includes(RecordStatus.UNPUBLISHED) ? { color: UNPUBLISHED_COLOR } : undefined}
+    />
   );
   
 };
