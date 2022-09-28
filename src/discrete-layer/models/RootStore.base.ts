cefadc4267a5adaf750995a76f66d53a826ae569
Alex Lebinson <newdarkvirus@gmail.com>
2022-09-28 18:45:36 +0300

    fix: abort job synch + remark job actions (#366)

--- src/discrete-layer/models/RootStore.base.ts
+++ src/discrete-layer/models/RootStore.base.ts
@@ -323,7 +323,8 @@ mutateStartRasterIngestion="mutateStartRasterIngestion",
 mutateStart3DIngestion="mutateStart3DIngestion",
 mutateStartDemIngestion="mutateStartDemIngestion",
 mutateUpdateJob="mutateUpdateJob",
-mutateJobRetry="mutateJobRetry"
+mutateJobRetry="mutateJobRetry",
+mutateJobAbort="mutateJobAbort"
 }
 
 /**
@@ -437,5 +438,8 @@ export const RootStoreBase = withTypedRefs<Refs>()(MSTGQLStore
     },
     mutateJobRetry(variables: { id: string }, optimisticUpdate?: () => void) {
       return self.mutate<{ jobRetry: string }>(`mutation jobRetry($id: String!) { jobRetry(id: $id) }`, variables, optimisticUpdate)
+    },
+    mutateJobAbort(variables: { id: string }, optimisticUpdate?: () => void) {
+      return self.mutate<{ jobAbort: string }>(`mutation jobAbort($id: String!) { jobAbort(id: $id) }`, variables, optimisticUpdate)
     },
   })))
