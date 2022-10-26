5d67246b0836193d727644a779416d06b87af10a
assafchamoy <assafchamoy97@gmail.com>
2022-10-26 15:30:29 +0300

    feat: added update geopkg query

--- src/discrete-layer/components/layer-details/entity.dialog.tsx
+++ src/discrete-layer/components/layer-details/entity.dialog.tsx
@@ -36,6 +36,7 @@ import {
   Layer3DRecordInput,
   LayerDemRecordInput,
   LayerRasterRecordInput,
+  RootStoreBaseMutations,
 } from '../../models/RootStore.base';
 import { UserAction } from '../../models/userStore';
 import {
@@ -187,6 +188,96 @@ export const EntityDialog: React.FC<EntityDialogProps> = observer(
       );
     };
 
+    const handleIngestQueries = useCallback((): void => {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      const { directory, fileNames, __typename, ...metadata } = inputValues;
+      switch (recordType) {
+        case RecordType.RECORD_DEM:
+          mutationQuery.setQuery(
+            store.mutateStartDemIngestion({
+              data: {
+                directory: directory as string,
+                fileNames: (fileNames as string).split(','),
+                metadata: metadata as LayerDemRecordInput,
+                type: RecordType.RECORD_DEM,
+              },
+            })
+          );
+          break;
+        case RecordType.RECORD_3D:
+          mutationQuery.setQuery(
+            store.mutateStart3DIngestion({
+              data: {
+                directory: directory as string,
+                fileNames: [fileNames as string],
+                metadata: metadata as Layer3DRecordInput,
+                type: RecordType.RECORD_3D,
+              },
+            })
+          );
+          break;
+        case RecordType.RECORD_RASTER:
+          mutationQuery.setQuery(
+            store.mutateStartRasterIngestion({
+              data: {
+                directory: directory as string,
+                fileNames: (fileNames as string).split(','),
+                metadata: metadata as LayerRasterRecordInput,
+                type: RecordType.RECORD_RASTER,
+              },
+            })
+          );
+          break;
+        default:
+          break;
+      }
+    }, [inputValues]);
+
+    const handleEditQueries = useCallback((): void => {
+      if (inputValues.__typename !== 'BestRecord') {
+        mutationQuery.setQuery(
+          store.mutateUpdateMetadata({
+            data: {
+              id: inputValues.id as string,
+              type: inputValues.type as RecordType,
+              partialRecordData: getPartialRecord(inputValues as Record<string, unknown> as Partial<ILayerImage>, descriptors as FieldConfigModelType[], IS_EDITABLE),
+            },
+          })
+        );
+      } else {
+        setTimeout(() => {
+          store.bestStore.editBest({
+            ...(inputValues as BestRecordModelType),
+            // @ts-ignore
+            sensors:
+              inputValues.sensors !== undefined
+                ? (JSON.parse(
+                    '[' + (inputValues.sensors as string) + ']'
+                  ) as string[])
+                : [],
+          });
+        }, IMMEDIATE_EXECUTION);
+        closeDialog();
+      }
+    }, [inputValues]);
+
+    const handleUpdateQueries = useCallback((): void => {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      const { directory, fileNames, __typename, ...metadata } = inputValues;
+      if(recordType === RecordType.RECORD_RASTER) {
+        mutationQuery.setQuery(
+          store.mutateStartRasterUpdateGeopkg({
+            data: {
+              directory: directory as string,
+              fileNames: (fileNames as string).split(','),
+              metadata: metadata as LayerRasterRecordInput,
+              type: RecordType.RECORD_RASTER,
+            },
+          })
+        );
+      }
+    }, [inputValues]);
+
     useEffect(() => {
       if (!isEmpty(descriptors) && !isEmpty(layerRecord)) {
         setIsAllInfoReady(true);
@@ -330,75 +421,17 @@ export const EntityDialog: React.FC<EntityDialogProps> = observer(
 
     useEffect(() => {
       if (vestValidationResults.errorCount === NONE) {
-        if (mode === Mode.EDIT) {
-          if (inputValues.__typename !== 'BestRecord') {
-            mutationQuery.setQuery(
-              store.mutateUpdateMetadata({
-                data: {
-                  id: inputValues.id as string,
-                  type: inputValues.type as RecordType,
-                  partialRecordData: getPartialRecord(inputValues as Record<string, unknown> as Partial<ILayerImage>, descriptors as unknown[] as FieldConfigModelType[], IS_EDITABLE),
-                },
-              })
-            );
-          } else {
-            setTimeout(() => {
-              store.bestStore.editBest({
-                ...(inputValues as BestRecordModelType),
-                // @ts-ignore
-                sensors:
-                  inputValues.sensors !== undefined
-                    ? (JSON.parse(
-                        '[' + (inputValues.sensors as string) + ']'
-                      ) as string[])
-                    : [],
-              });
-            }, IMMEDIATE_EXECUTION);
-            closeDialog();
-          }
-        } else {
-          // eslint-disable-next-line @typescript-eslint/naming-convention
-          const { directory, fileNames, __typename, ...metadata } = inputValues;
-          switch (recordType) {
-            case RecordType.RECORD_DEM:
-              mutationQuery.setQuery(
-                store.mutateStartDemIngestion({
-                  data: {
-                    directory: directory as string,
-                    fileNames: (fileNames as string).split(','),
-                    metadata: metadata as LayerDemRecordInput,
-                    type: RecordType.RECORD_DEM,
-                  },
-                })
-              );
-              break;
-            case RecordType.RECORD_3D:
-              mutationQuery.setQuery(
-                store.mutateStart3DIngestion({
-                  data: {
-                    directory: directory as string,
-                    fileNames: [fileNames as string],
-                    metadata: metadata as Layer3DRecordInput,
-                    type: RecordType.RECORD_3D,
-                  },
-                })
-              );
-              break;
-            case RecordType.RECORD_RASTER:
-              mutationQuery.setQuery(
-                store.mutateStartRasterIngestion({
-                  data: {
-                    directory: directory as string,
-                    fileNames: (fileNames as string).split(','),
-                    metadata: metadata as LayerRasterRecordInput,
-                    type: RecordType.RECORD_RASTER,
-                  },
-                })
-              );
-              break;
-            default:
-              break;
-          }
+        // NEW
+        switch(mode) {
+          case Mode.NEW:
+            handleIngestQueries();
+          break;
+          case Mode.EDIT:
+            handleEditQueries();
+          break;
+          case Mode.UPDATE:
+            handleUpdateQueries();
+          break;
         }
       }
     }, [vestValidationResults]);
@@ -408,8 +441,23 @@ export const EntityDialog: React.FC<EntityDialogProps> = observer(
     }, [onSetOpen]);
 
     useEffect(() => {
-      // @ts-ignore
-      if (!mutationQuery.loading && (mutationQuery.data?.updateMetadata === 'ok' || mutationQuery.data?.start3DIngestion === 'ok' || mutationQuery.data?.startRasterIngestion === 'ok')) {
+      const SUCCESS_RESPONSE_VAL = 'ok';
+
+      const mutationServices = Object.keys(RootStoreBaseMutations)
+      .map(m => {
+        // Converting query names into actual property names of the resolved data.
+        // So it can be matched later with the returned data.
+        const MUTATION_QUERY_PREFIX = 'mutate';
+
+        const actualMutationName = m.replace(MUTATION_QUERY_PREFIX,'');
+        const lowerFirstLetter = actualMutationName.charAt(0).toLowerCase() + actualMutationName.slice(1);
+
+        return lowerFirstLetter;
+    });
+      const hasAnyQuerySucceeded = Object.entries(mutationQuery.data ?? {})
+      .some(([key, val]) => mutationServices.includes(key) && val === SUCCESS_RESPONSE_VAL);
+
+      if (!mutationQuery.loading && hasAnyQuerySucceeded) {
         closeDialog();
         
         dispatchAction({ 
