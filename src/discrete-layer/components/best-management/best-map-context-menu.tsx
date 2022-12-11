6cc3cf89b8bf053a495ea8cb579e4bb04189cfb3
Assaf Chamoy <assafchamoy97@gmail.com>
2022-12-11 19:33:17 +0200

    feat: refactor context menu to be more generic

--- src/discrete-layer/components/best-management/best-map-context-menu.tsx
+++ src/discrete-layer/components/best-management/best-map-context-menu.tsx
@@ -1,55 +1,179 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import { useIntl } from 'react-intl';
-import { IContextMenuData } from '@map-colonies/react-components';
+import { Box, IContextMenuData } from '@map-colonies/react-components';
 import { IActionGroup } from '../../../common/actions/entity.actions';
 import { TabViews } from '../../views/tab-views';
 import { useStore } from '../../models/RootStore';
 import { ContextMenu } from '../map-container/context-menu';
+import { get } from 'lodash';
+import { Icon, Tooltip } from '@map-colonies/react-core';
 
 interface IBestMapContextMenuData extends IContextMenuData {
   entityTypeName: string;
 }
 
+const NONE = 0;
+const FIRST = 0;
+
 export const BestMapContextMenu: React.FC<IBestMapContextMenuData> = ({
   entityTypeName,
   ...restProps
 }) => {
-
+  const { data, position, handleClose } = restProps;
   const store = useStore();
   const intl = useIntl();
-  
-  const entityName = store.actionDispatcherStore.getEntityActionConfiguration(entityTypeName)?.childEntity;
-  
+
+  const dispatchAction = (
+    action: string,
+    data: Record<string, unknown>
+  ): void => {
+    store.actionDispatcherStore.dispatchAction({ action, data });
+  };
+
+  const [
+    isOverlappingLayersExpanded,
+    setIsOverlappingLayersExpanded,
+  ] = useState<boolean>(false);
+
+  const entityName = store.actionDispatcherStore.getEntityActionConfiguration(
+    entityTypeName
+  )?.childEntity;
+  const layer = get(data, '[0].meta') as Record<string, unknown>;
+  const layerName = get(layer, 'details.name') as string;
+  const numOfSelectedLayers = get(data, 'length');
+
+  const subTitle = intl.formatMessage({ id: 'context-menu.sub-title.tooltip' });
+  const tooltipInfoPrefixText = intl.formatMessage({
+    id: 'context-menu.title.tooltip',
+  });
+
   const entityPermittedActions = useMemo(() => {
     if (entityName !== undefined) {
-      const allGroupsActions = store.actionDispatcherStore.getEntityActionGroups(entityName).filter(actionGroup => actionGroup.type === 'mapActions');
+      const allGroupsActions = store.actionDispatcherStore
+        .getEntityActionGroups(entityName)
+        .filter((actionGroup) => actionGroup.type === 'mapActions');
       return allGroupsActions.map((actionGroup) => {
         return {
           titleTranslationId: actionGroup.titleTranslationId,
-          group: 
-            actionGroup.group.filter(action => {
-              // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-              return store.userStore.isActionAllowed(`entity_action.${entityName}.${action.action}`) === false ? false : true &&
-                    action.views.includes(TabViews.CREATE_BEST);
+          group: actionGroup.group
+            .filter((action) => {
+              return (store.userStore.isActionAllowed(`entity_action.${entityName}.${action.action}`) === true && action.views.includes(TabViews.CREATE_BEST)
+              );
             })
             .map((action) => {
               return {
                 ...action,
-                titleTranslationId: intl.formatMessage({ id: action.titleTranslationId }),
+                titleTranslationId: intl.formatMessage({
+                  id: action.titleTranslationId,
+                }),
               };
             }),
-        }
+        };
       });
-    }
-    else {
+    } else {
       return [];
     }
-  
   }, []);
 
-  const flatPermittedActions = (entityPermittedActions as IActionGroup[])[0].group;
+  const flatPermittedActions = (entityPermittedActions as IActionGroup[])[0]
+    .group;
+
+  const actionsSection = flatPermittedActions.map((action) => {
+    return (
+      <Box
+        onClick={(evt): void => {
+          dispatchAction(`LayerRasterRecord.${action.action}`, layer);
+          handleClose();
+        }}
+      >
+        <Icon
+          className="imageryMenuIcon glow-missing-icon"
+          icon={{ icon: action.icon, size: 'small' }}
+        />
+        {action.titleTranslationId}
+      </Box>
+    );
+  });
+
+  const renderOverlappingSection = (): JSX.Element | false => {
+    return (
+      numOfSelectedLayers > 1 && (
+        <Box>
+          <Box className="imageryContextMenuSubTitle">
+            {`${numOfSelectedLayers} ${subTitle}`}{' '}
+            <Icon
+              className="imageryContextMenuSubTitleIcon"
+              icon={{
+                icon: !isOverlappingLayersExpanded
+                  ? 'arrow_drop_down'
+                  : 'arrow_drop_up',
+              }}
+              onClick={(evt: React.MouseEvent<HTMLElement>): void => {
+                evt.stopPropagation();
+                setIsOverlappingLayersExpanded((prev) => !prev);
+              }}
+            />
+          </Box>
+          {isOverlappingLayersExpanded && (
+            <Box className="imageryContextMenuTableContainer">
+              <table className="imageryContextMenuTable">
+                {data.map((item: Record<string, unknown>, index: number) => {
+                  const meta = item.meta as Record<string, unknown>;
+                  const details = meta.details as Record<string, unknown>;
+                  return (
+                    <tr
+                      className={
+                        index === FIRST
+                          ? 'imageryContextMenuTableSelectedRow'
+                          : ''
+                      }
+                    >
+                      <td className="imageryContextMenuTableOrder">
+                        {meta.zIndex as number}
+                      </td>
+                      <td>
+                        <Tooltip content={details.name as string}>
+                          <Box className="imageryContextMenuTableField">
+                            {details.name as string}
+                          </Box>
+                        </Tooltip>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </table>
+            </Box>
+          )}
+        </Box>
+      )
+    );
+  };
+
+  const emptyContainerStyle = {
+    left: `${position.x}px`,
+    top: `${position.y}px`,
+  };
 
   return (
-    <ContextMenu actions={flatPermittedActions} {...restProps} />
+    <>
+      {numOfSelectedLayers > 1 && (
+        <ContextMenu
+          menuTitle={layerName}
+          menuTitleTooltip={tooltipInfoPrefixText + layerName}
+          menuSections={[actionsSection]}
+          // Multi sections example
+          // menuSections={[actionsSection, [<Box>One</Box>, <Box onClick={handleClose}>Two</Box>, <Box onClick={handleClose}>Three</Box>]]}
+          {...restProps}
+        >
+          {renderOverlappingSection()}
+          {numOfSelectedLayers === NONE && (
+            <Box
+              style={emptyContainerStyle}
+              className="imageryContextMenuEmpty"
+            ></Box>
+          )}
+        </ContextMenu>
+      )}
+    </>
   );
 };
--- src/discrete-layer/components/map-container/context-menu.tsx
+++ src/discrete-layer/components/map-container/context-menu.tsx
@@ -1,46 +1,46 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { useIntl } from 'react-intl';
+import React, { MouseEventHandler, useEffect, useRef } from 'react';
 import { get } from 'lodash';
-import { Icon, Menu, MenuItem, MenuSurfaceAnchor, Tooltip } from '@map-colonies/react-core';
+import {
+  Icon,
+  ListDivider,
+  Menu,
+  MenuItem,
+  MenuSurfaceAnchor,
+  Tooltip,
+} from '@map-colonies/react-core';
 import { Box, IContextMenuData } from '@map-colonies/react-components';
-import { IAction } from '../../../common/actions/entity.actions';
-import { useStore } from '../../models/RootStore';
 
 import './context-menu.css';
 
-const NONE = 0;
-const FIRST = 0;
 const TITLE_HEIGHT = 24;
 const SUB_TITLE_HEIGHT = 24;
 const MARGIN_HEIGHT = 20;
 
 interface IMapContextMenuData extends IContextMenuData {
-  actions: IAction[];
+  menuTitle?: string;
+  menuTitleTooltip?: string;
+  menuSections: JSX.Element[][];
 }
 
+// Children prop isn't rendered as a part of the menu, but as a separated bottom section.
 export const ContextMenu: React.FC<IMapContextMenuData> = ({
-  data,
   position,
   style,
   size,
   handleClose,
-  actions
+  menuSections,
+  menuTitle = '',
+  menuTitleTooltip = '',
+  children,
 }) => {
-
-  const store = useStore();
-  const intl = useIntl();
   const imageryContextMenuRef = useRef(null);
-  const [expanded, setExpanded] = useState<boolean>(false);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent): void => {
       /* eslint-disable */
       const target: any = event.target;
       const imgContextMenuRef: any = get(imageryContextMenuRef, 'current');
-      if (
-        imgContextMenuRef &&
-        !imgContextMenuRef.contains(target)
-      ) {
+      if (imgContextMenuRef && !imgContextMenuRef.contains(target)) {
         document.removeEventListener('click', handleClickOutside, false);
         handleClose();
       }
@@ -49,115 +49,70 @@ export const ContextMenu: React.FC<IMapContextMenuData> = ({
 
     document.addEventListener('click', handleClickOutside, false);
   });
-  
-  const emptyStyle = {
-    left: `${position.x}px`,
-    top: `${position.y}px`
-  };
-  const layer = get(data,'[0].meta') as Record<string, unknown>;
-  const layerName = get(layer,'details.name') as string;
-  const info = intl.formatMessage({ id: 'context-menu.title.tooltip' });
-  const subTitle = intl.formatMessage({ id: 'context-menu.sub-title.tooltip' });
-  // eslint-disable-next-line
-  const numOfSelectedLayers = get(data,'length') as number;
 
-  const dispatchAction = (
-    action: string,
-    data: Record<string, unknown>
-  ): void => {
-    store.actionDispatcherStore.dispatchAction({ action, data });
+  const baseStyle = {
+    left: `${position.x}px`,
+    top: `${position.y}px`,
   };
 
   return (
     <>
       {
-        numOfSelectedLayers > NONE &&
-        <div 
+        <div
           ref={imageryContextMenuRef}
-          style={{...emptyStyle, ...style}}
+          style={{ ...baseStyle }}
           className="imageryContextMenuTheme imageryContextMenu"
         >
           <Box>
-            <Box className="imageryContextMenuTitle">{layerName}</Box>
-            {' '}
-            <Tooltip content={info + layerName}>
-              <Icon
-                className="imageryContextMenuTitleInfo"
-                icon={{ icon: 'info', size: 'small' }}
-              />
-            </Tooltip>
+            {menuTitle && (
+              <Box className="imageryContextMenuTitle">{`${menuTitle} `}</Box>
+            )}
+            {menuTitle && menuTitleTooltip && (
+              <Tooltip content={menuTitleTooltip}>
+                <Icon
+                  className="imageryContextMenuTitleInfo"
+                  icon={{ icon: 'info', size: 'small' }}
+                />
+              </Tooltip>
+            )}
           </Box>
-          <MenuSurfaceAnchor style={{height: `calc(${(size as Record<string, number>).height}px - ${TITLE_HEIGHT}px - ${SUB_TITLE_HEIGHT}px - ${MARGIN_HEIGHT}px)`}}>
-            <Menu
-              open={true}
-              className="imageryMenu"
-            >
-              {
-                actions.map((action: IAction) => {
-                  return (
-                    <MenuItem key={`imageryMenuItemAction_${action.action}`}>
-                      <Box
-                        onClick={(evt): void => {
-                          dispatchAction(`LayerRasterRecord.${action.action}`, layer);
-                          handleClose();
-                        }}
+
+          <MenuSurfaceAnchor
+            className='menuAnchor'
+            // style={{
+            //   height: `calc(${
+            //     (size as Record<string, number>).height
+            //   }px - ${TITLE_HEIGHT}px - ${SUB_TITLE_HEIGHT}px - ${MARGIN_HEIGHT}px)`,
+            // }}
+          >
+            <Menu open={true} className="imageryMenu">
+              {menuSections.map((section, sectionIdx) => {        
+                 const sectionItems = section.map((item, itemIdx) => {
+                    // Get click callback from item
+                    const menuItemClick = (item.props as Record<string, unknown>).onClick ?? ((): void => { return });
+                    return (
+                      <MenuItem
+                        key={`imageryMenuItemAction_${sectionIdx}_${itemIdx}`}
+                        onClick={menuItemClick as MouseEventHandler<HTMLElement> | MouseEventHandler<HTMLDivElement>}
                       >
-                        <Icon
-                          className="imageryMenuIcon glow-missing-icon"
-                          icon={{ icon: action.icon, size: 'small' }}
-                        />
-                        {action.titleTranslationId}
-                      </Box>
-                    </MenuItem>
-                  );
-                })
-              }
+                        {item}
+                      </MenuItem>
+                    )
+                  });
+
+                  const lastSectionIdx = menuSections.length - 1;
+                  if(sectionIdx < lastSectionIdx && section.length) {
+                    sectionItems.push(<ListDivider className='sectionDivider'/>);
+                  }
+
+                  return sectionItems;
+              })}
+                
             </Menu>
           </MenuSurfaceAnchor>
-          {
-            numOfSelectedLayers > 1 &&
-            <Box>
-              <Box className="imageryContextMenuSubTitle">
-                {`${numOfSelectedLayers} ${subTitle}`}
-                {' '}
-                <Icon
-                  className="imageryContextMenuSubTitleIcon"
-                  icon={{ icon: !expanded ? 'arrow_drop_down' : 'arrow_drop_up' }}
-                  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-                  onClick={(evt): void => {
-                    evt.stopPropagation();
-                    setExpanded(!expanded);
-                  }}
-                />
-              </Box>
-              {
-                expanded &&
-                <Box className="imageryContextMenuTableContainer">
-                  <table className="imageryContextMenuTable">
-                    {
-                      data.map((item: Record<string, unknown>, index: number) => {
-                        const meta = item.meta as Record<string, unknown>;
-                        const details = meta.details as Record<string, unknown>;
-                        return (
-                          <tr className={index === FIRST ? 'imageryContextMenuTableSelectedRow' : ''}>
-                            <td className="imageryContextMenuTableOrder">{meta.zIndex as number}</td>
-                            <td>
-                              <Tooltip content={details.name as string}>
-                                <Box className="imageryContextMenuTableField">{details.name as string}</Box>
-                              </Tooltip>
-                            </td>
-                          </tr>
-                        );
-                      })
-                    }
-                  </table>
-                </Box>
-              }
-            </Box>
-          }
+          {children}
         </div>
       }
-      {numOfSelectedLayers === NONE && <Box style={emptyStyle} className="imageryContextMenuEmpty"></Box>}
     </>
   );
 };
