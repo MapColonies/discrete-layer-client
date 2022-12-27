d79fb0f7b703e21d45cda9621a095916e3b9699c
ronnahom96 <ronnahom96@gmail.com>
2022-12-27 15:06:26 +0200

    feat: build fetcher for lookup tables and use context

---
+++ src/discrete-layer/views/components/data-fetchers/lookup-tables-fetcher.component.tsx
@@ -0,0 +1,27 @@
+import { observer } from 'mobx-react';
+import React, { useContext, useEffect } from 'react';
+import lookupTablesContext from '../../../../common/contexts/lookupTables.context';
+import { LookupTablesDataModelType } from '../../../models';
+import { useQuery, useStore } from '../../../models/RootStore';
+
+export const LookupTablesFetcher: React.FC = observer(() => {
+  const store = useStore();
+  const { data, loading } = useQuery((store) => store.queryGetLookupTablesData({},
+    `classificationList
+      countryList
+  `));
+
+  const { setLookupTablesData } = useContext(lookupTablesContext);
+
+  useEffect(() => {
+    if (!loading) {
+      const lookupTablesData = data?.getLookupTablesData as LookupTablesDataModelType;
+      console.log(lookupTablesData)
+      setLookupTablesData(lookupTablesData);
+    }
+  }, [data, loading, store.discreteLayersStore]);
+
+  return (
+    <></>
+  );
+});
