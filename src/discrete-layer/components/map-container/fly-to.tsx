168725acdd42c701339a03fba6469c8feaab9e16
EllaMartirosyan <34953077+EllaMartirosyan@users.noreply.github.com>
2023-01-11 14:28:41 +0200

    feat: set 3d scene mode in case of fly to a 3d model (#406)
    * feat: set 3d scene mode in case of fly to a 3d model
    
    * fix: code review
    
    * fix: fix
    
    * fix: zoom instead of fly
    
    * fix: build errors
    
    * fix: use rectangle which is clamped to terrain instead of volume with fixed incorrect height

--- src/discrete-layer/components/map-container/fly-to.tsx
+++ src/discrete-layer/components/map-container/fly-to.tsx
@@ -1,16 +1,37 @@
 import React, { useEffect } from 'react';
-import { CesiumRectangle, useCesiumMap } from '@map-colonies/react-components';
+import { CesiumColor, CesiumRectangle, CesiumSceneMode, useCesiumMap } from '@map-colonies/react-components';
+import { LayerMetadataMixedUnion, RecordType } from '../../models';
+
+const IMMEDIATELY = 0;
+const TRANSPARENT = 0.0;
 
 interface FlyToProps {
   rect: CesiumRectangle;
   setRect: (rect: CesiumRectangle | undefined) => void;
+  layer: LayerMetadataMixedUnion;
 }
 
-export const FlyTo: React.FC<FlyToProps> = ({ rect, setRect}): JSX.Element => {
+export const FlyTo: React.FC<FlyToProps> = ({ rect, setRect, layer}): JSX.Element => {
   const mapViewer = useCesiumMap();
 
   useEffect(() => {
-    mapViewer.camera.flyTo({ destination: rect });
+    if (mapViewer.scene.mode !== CesiumSceneMode.SCENE3D && layer.type === RecordType.RECORD_3D) {
+      mapViewer.scene.morphTo3D(IMMEDIATELY);
+    }
+
+    const rectangle = mapViewer.entities.add({
+      rectangle: {
+        coordinates: rect,
+        material: CesiumColor.PURPLE.withAlpha(TRANSPARENT)
+      },
+    });
+    
+    void mapViewer.flyTo(
+      rectangle,
+    ).then(() => {
+      mapViewer.entities.remove(rectangle);
+    });
+
     setRect(undefined);
   }, []);
 
