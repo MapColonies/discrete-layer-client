ac6daf7f29a9d8ee449f3fd00281f8d0247c68fb
ronnahom96 <ronnahom96@gmail.com>
2023-01-15 10:14:44 +0200

    add error logic, we must to disable eslint here or refactor a lot of thing from the system

--- src/discrete-layer/views/components/data-fetchers/lookup-tables-fetcher.component.tsx
+++ src/discrete-layer/views/components/data-fetchers/lookup-tables-fetcher.component.tsx
@@ -1,12 +1,14 @@
+/* eslint-disable */
 import { observer } from 'mobx-react';
 import React, { useContext, useEffect } from 'react';
 import lookupTablesContext, { ILookupTableData } from '../../../../common/contexts/lookupTables.context';
-import { useQuery, useStore } from '../../../models/RootStore';
+import { useStore, useQuery } from '../../../models/RootStore';
+import { Error } from './../../../../common/components/tree/statuses/error';
+import { queue } from '../../../../discrete-layer/components/snackbar/notification-queue';
 
 export const LookupTablesFetcher: React.FC = observer(() => {
   const store = useStore();
-  const { data, loading } = useQuery((store) => store.queryGetLookupTablesData({}, `dictionary`));
-
+  const { data, loading, error } = useQuery((store) => store.queryGetLookupTablesData({}, `dictionary`))
   const { setLookupTablesData } = useContext(lookupTablesContext);
 
   useEffect(() => {
@@ -14,6 +16,20 @@ export const LookupTablesFetcher: React.FC = observer(() => {
       setLookupTablesData(data?.getLookupTablesData as ILookupTableData);
     }
   }, [data, loading, store.discreteLayersStore]);
+
+  useEffect(() => {
+    if (error) {
+      queue.notify({
+        body: (
+          <Error
+            className="errorNotification"
+            message={error.response?.errors[0].message}
+            details={error.response?.errors[0].extensions?.exception?.config?.url}
+          />
+        )
+      })
+    }
+  }, [error])
 
   return null;
 });
