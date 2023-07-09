826465ca4030b56af1cf3f81d56176e701021e6b
assafchamoy <assafchamoy97@gmail.com>
2023-07-09 18:46:20 +0300

    fix: bugs #1

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,6 @@
 const webpack = require('webpack');
+const path = require('path');
+
 module.exports = function override(config, env) {
     //do stuff with the webpack config...
 
@@ -13,6 +15,13 @@ module.exports = function override(config, env) {
         stream: require.resolve('stream-browserify'),
         zlib: require.resolve("browserify-zlib")
     };
+
+    config.resolve.alias = {
+        ...config.resolve.alias,
+        'react-dnd': path.resolve(__dirname, './node_modules/react-dnd'),
+        'react-dnd-html5-backend': path.resolve(__dirname, './node_modules/react-dnd-html5-backend'),
+    }
+    
     config.plugins.push(
         new webpack.ProvidePlugin({
             process: 'process/browser',
