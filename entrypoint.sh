541980cfc6bad635162cbbd438935fcda01454e6
alebinson <newdarkvirus@gmail.com>
2024-02-20 12:33:06 +0200

    fix: entrypoint special chars

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
+echo "Running CONFD ..."  && \
 node ../confd/generate-config.js --indocker && \
+echo "Preparing SED command -->" 's/{PUBLIC_URL_PLACEHOLDER}/'"$(echo "$CONFIGURATION_PUBLIC_URL" | sed 's/[^a-zA-Z0-9.]/\\&/g')"'/g'  && \
 sed -i 's/{PUBLIC_URL_PLACEHOLDER}/'"$CONFIGURATION_PUBLIC_URL"'/g' ./static/js/main.*.js && \
+echo "Running NGINX ..." && \
 nginx -g "daemon off;"
