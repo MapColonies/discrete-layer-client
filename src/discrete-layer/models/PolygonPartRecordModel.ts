921a0bf0c0dd6106bfe7d9ff1e99fefa3e242685
alebinson <newdarkvirus@gmail.com>
2024-04-02 13:55:14 +0300

    fix: normilize pp entity for client

--- src/discrete-layer/models/PolygonPartRecordModel.ts
+++ src/discrete-layer/models/PolygonPartRecordModel.ts
@@ -1,6 +1,9 @@
 import { Instance, types } from "mobx-state-tree"
 import { PolygonPartRecordModelBase } from "./PolygonPartRecordModel.base"
 import { momentDateType } from "./moment-date.type"
+import { ProductTypeEnumType } from "./ProductTypeEnum";
+import { LinkModel } from "./LinkModel";
+import { RecordTypeEnumType } from "./RecordTypeEnum";
 
 /* The TypeScript type of an instance of PolygonPartRecordModel */
 export interface PolygonPartRecordModelType extends Instance<typeof PolygonPartRecordModel.Type> {}
@@ -31,7 +34,18 @@ export const PolygonPartRecordModel = PolygonPartRecordModelBase
     imagingTimeEndUTC: types.maybe(momentDateType),
     /* tslint:enable */
     /* eslint-enable */
+
+    // **** DUMMY FIELDS(ENTITY LEVEL) TO NORMALIZE POLYGON PARTS RECORD (WILL BE ALWAYS EMPTY)
+    id: types.identifier, //Alex change till proper deffs
+    productType: types.union(types.undefined, ProductTypeEnumType),
+    productName: types.union(types.undefined, types.string),
+    footprint: types.union(types.undefined, types.frozen()),
+    links: types.union(types.undefined, types.null, types.array(types.late((): any => LinkModel))),
+    type: types.union(types.undefined, types.null, RecordTypeEnumType),
+    
+    // **** DUMMY FIELDS(CLENT OPERATIONAL LEVEL)
     footprintShown: types.union(types.undefined, types.null, types.boolean),
     order: types.union(types.undefined, types.null, types.number),
     layerImageShown: types.union(types.undefined, types.null, types.boolean),
+
   })
