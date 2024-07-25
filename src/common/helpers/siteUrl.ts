db7dcfbd4e74aa485c526a7518114b385c85e67e
Dorin-RFL <rutime7@gmail.com>
2024-07-25 15:09:13 +0300

    fix: changing of function name and user configuration value

--- src/common/helpers/siteUrl.ts
+++ src/common/helpers/siteUrl.ts
@@ -1,11 +1,11 @@
 import CONFIG from '../config';
 
-export const currentClient = `${CONFIG.SERVICE_PROTOCOL as string}${
+export const currentClientUrl = `${CONFIG.SERVICE_PROTOCOL as string}${
   CONFIG.SERVICE_NAME as string
 }`;
 
 export const currentSite = () => {
-  return CONFIG.SITES_CONFIG.slaves.includes(currentClient)
+  return CONFIG.SITES_CONFIG.slaves.includes(currentClientUrl)
     ? 'slave'
     : 'master';
 };
