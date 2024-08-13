952619a6f85ea1902556accde97e9a8c9b608e81
Ella Martirosyan <34953077+EllaMartirosyan@users.noreply.github.com>
2024-08-13 18:09:21 +0300

    fix: config generation (#516)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,31 @@
-echo "Sleeping for 3 secs ..."  && \
-sleep 3  && \
-echo "Running CONFD ..."  && \
-node ../confd/generate-config.js --indocker && \
-echo "Preparing SED command -->" 's/{PUBLIC_URL_PLACEHOLDER}/'"$(echo "$CONFIGURATION_PUBLIC_URL" | sed 's/[^a-zA-Z0-9.]/\\&/g')"'/g'  && \
-sed -i 's/{PUBLIC_URL_PLACEHOLDER}/'"$(echo "$CONFIGURATION_PUBLIC_URL" | sed 's/[^a-zA-Z0-9.]/\\&/g')"'/g' ./static/js/main.*.js && \
-echo "Running NGINX ..." && \
-nginx -g "daemon off;"
+#!/bin/sh
+
+set -e
+
+node ../confd/generate-config.js --indocker
+
+# Check if env-config.js contains "{{" or has size 0
+if grep -q '{{' env-config.js || [ ! -s env-config.js ]; then
+    echo "Error: env-config.js contains '{{' or is empty"
+    exit 1
+fi
+
+# Check if index.html contains "{{" or has size 0
+if grep -q '{{' index.html || [ ! -s index.html ]; then
+    echo "Error: index.html contains '{{' or is empty"
+    exit 1
+fi
+
+echo "Done with confd"
+
+
+echo "Running SED command -->" 's/{PUBLIC_URL_PLACEHOLDER}/'"$(echo "$CONFIGURATION_PUBLIC_URL" | sed 's/[^a-zA-Z0-9.]/\\&/g')"'/g'
+
+sed -i 's/{PUBLIC_URL_PLACEHOLDER}/'"$(echo "$CONFIGURATION_PUBLIC_URL" | sed 's/[^a-zA-Z0-9.]/\\&/g')"'/g' ./static/js/main.*.js
+
+echo "Done with SED command"
+
+
+echo "Running NGINX ..."
+
+nginx -g 'daemon off;'
