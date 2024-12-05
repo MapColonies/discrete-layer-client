56d694cb036ce039392825548239c7dfa3697e10
Eliav Maimon <135841794+Eliav-Maimon@users.noreply.github.com>
2024-12-05 15:40:33 +0200

    fix: Ingestion inputs values (directory and file) should not push buttons left (MAPCO-4191) (#547)
    * fix: when path and file name are too long use ellipsis
    
    * fix: not allowing multipule selection in raster filePicker
    
    * fix: pr
    
    ---------
    
    Co-authored-by: Alex Lebinson <newdarkvirus@gmail.com>

--- src/discrete-layer/components/layer-details/ingestion-fields.tsx
+++ src/discrete-layer/components/layer-details/ingestion-fields.tsx
@@ -52,7 +52,7 @@ const FileItem: React.FC<{ file: FileData }> = ({ file }) => {
   return (
     <>
       <Box><Icon className="fileIcon mc-icon-Map-Vector" /></Box>
-      <Box>{file.name}</Box>
+      <Box className='fileItemName'>{file.name}</Box>
       <Box style={{ direction: 'ltr' }}>
         {defaultFormatters.formatFileSize(null, file)}
       </Box>
@@ -115,9 +115,9 @@ const IngestionInputs: React.FC<{
                 {
                   index === DIRECTORY && values[index] !== '' &&
                   <Tooltip content={values[index]}>
-                    <Typography tag="div" dir="auto" className="filesPathContainer">
-                      { values[index] }
-                    </Typography>
+                    <Box className="filesPathContainer">
+                      <Typography dir='ltr' tag='span'>{values[index]}</Typography>
+                    </Box>
                   </Tooltip>
                 }
                 {
