3895dc268cdeff116ddf542b363b60e849e8be93
Eliav Maimon <liavma3@gmail.com>
2025-03-16 11:39:24 +0200

    feat: add vector catalog to the tree

--- src/discrete-layer/components/layer-details/utils.ts
+++ src/discrete-layer/components/layer-details/utils.ts
@@ -238,7 +238,7 @@ const checkIsBest = (entity: ILayerImage): boolean => {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   const { ORTHOPHOTO_BEST, RASTER_AID_BEST, RASTER_MAP_BEST, RASTER_VECTOR_BEST, VECTOR_BEST, QUANTIZED_MESH_DTM_BEST, QUANTIZED_MESH_DSM_BEST } = ProductType;
 
-  const bestProductTypes: ProductType[] = [ ORTHOPHOTO_BEST, RASTER_AID_BEST, RASTER_MAP_BEST, RASTER_VECTOR_BEST, VECTOR_BEST, QUANTIZED_MESH_DTM_BEST, QUANTIZED_MESH_DSM_BEST ];
+  const bestProductTypes: ProductType[] = [ ORTHOPHOTO_BEST, RASTER_AID_BEST, RASTER_MAP_BEST, RASTER_VECTOR_BEST, QUANTIZED_MESH_DTM_BEST, QUANTIZED_MESH_DSM_BEST ];
 
   return bestProductTypes.includes(entity.productType as ProductType);
 };
@@ -249,6 +249,14 @@ export const isDiscrete = (entity: ILayerImage): boolean => {
 
 export const isBest = (entity: ILayerImage): boolean => {
   return checkIsBest(entity)
+};
+
+export const isVector = (entity: ILayerImage): boolean => {
+  const { VECTOR_BEST } = ProductType;
+
+  const vectorProductTypes: ProductType[] = [ VECTOR_BEST ];
+ 
+  return vectorProductTypes.includes(entity.productType as ProductType);
 };
 
 export const isMultiSelection = (recordType: RecordType): boolean => {
---
+++ src/discrete-layer/models/VectorFeatureTypeStructureModel.base.ts
@@ -0,0 +1,40 @@
+/* This is a mst-gql generated file, don't modify it manually */
+/* eslint-disable */
+/* tslint:disable */
+
+import { types } from "mobx-state-tree"
+import { QueryBuilder } from "mst-gql"
+import { ModelBase } from "./ModelBase"
+import { FieldFeatureTypeModel, FieldFeatureTypeModelType } from "./FieldFeatureTypeModel"
+import { FieldFeatureTypeModelSelector } from "./FieldFeatureTypeModel.base"
+import { RootStoreType } from "./index"
+
+
+/**
+ * VectorFeatureTypeStructureBase
+ * auto generated base class for the model VectorFeatureTypeStructureModel.
+ */
+export const VectorFeatureTypeStructureModelBase = ModelBase
+  .named('VectorFeatureTypeStructure')
+  .props({
+    __typename: types.optional(types.literal("VectorFeatureTypeStructure"), "VectorFeatureTypeStructure"),
+    layerName: types.union(types.undefined, types.null, types.string),
+    aliasLayerName: types.union(types.undefined, types.null, types.string),
+    fields: types.union(types.undefined, types.null, types.array(types.late((): any => FieldFeatureTypeModel))),
+  })
+  .views(self => ({
+    get store() {
+      return self.__getStore<RootStoreType>()
+    }
+  }))
+
+export class VectorFeatureTypeStructureModelSelector extends QueryBuilder {
+  get layerName() { return this.__attr(`layerName`) }
+  get aliasLayerName() { return this.__attr(`aliasLayerName`) }
+  fields(builder?: string | FieldFeatureTypeModelSelector | ((selector: FieldFeatureTypeModelSelector) => FieldFeatureTypeModelSelector)) { return this.__child(`fields`, FieldFeatureTypeModelSelector, builder) }
+}
+export function selectFromVectorFeatureTypeStructure() {
+  return new VectorFeatureTypeStructureModelSelector()
+}
+
+export const vectorFeatureTypeStructureModelPrimitives = selectFromVectorFeatureTypeStructure().layerName.aliasLayerName
