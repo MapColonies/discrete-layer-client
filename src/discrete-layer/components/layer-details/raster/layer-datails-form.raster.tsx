da3c495250345f5702b42d3f074feeb079c54774
Ella Martirosyan <34953077+EllaMartirosyan@users.noreply.github.com>
2025-05-28 18:16:13 +0300

    fix: manual part vertex density tollerance in degrees (#609)

--- src/discrete-layer/components/layer-details/raster/layer-datails-form.raster.tsx
+++ src/discrete-layer/components/layer-details/raster/layer-datails-form.raster.tsx
@@ -27,7 +27,7 @@ import { getGraphQLPayloadNestedObjectErrors, GraphQLError } from '../../../../c
 import { MetadataFile } from '../../../../common/components/file-picker';
 import { emphasizeByHTML } from '../../../../common/helpers/formatters';
 import { Loading } from '../../../../common/components/tree/statuses/loading';
-import { area, countSmallHoles, explode, geoArgs, getFirstPoint, getOutlinedFeature, isGeometryPolygon, isSmallArea, polygonVertexDensityFactor } from '../../../../common/utils/geo.tools';
+import { area, countSmallHoles, DEGREES_PER_METER, explode, geoArgs, getFirstPoint, getOutlinedFeature, isGeometryPolygon, isSmallArea, polygonVertexDensityFactor } from '../../../../common/utils/geo.tools';
 import { mergeRecursive, removePropertiesWithPrefix } from '../../../../common/helpers/object';
 import { useZoomLevels } from '../../../../common/hooks/useZoomLevels';
 import { useEnums } from '../../../../common/hooks/useEnum.hook';
@@ -904,8 +904,8 @@ export const InnerRasterForm = (
     if (!resolutionObj || !resolutionObj.value) {
       return resolutionNotExist();
     };
-
-    const densityFactor = polygonVertexDensityFactor(geometry, resolutionObj.value);
+ 
+    const densityFactor = polygonVertexDensityFactor(geometry, resolutionObj.value * DEGREES_PER_METER);
     if (densityFactor < CONFIG.POLYGON_PARTS.DENSITY_FACTOR) {
       return {
         valid: false,
